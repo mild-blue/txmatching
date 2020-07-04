6badd1c947931345c8126adbd3903d5db22777a0
Lukas Forst <lukas@forst.pw>
2020-07-04 20:02:25 +0200

    Add flask with db and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ RUN conda env create -f conda.yml
 # all RUN commands from now use the kidney-exchange environment
 SHELL ["conda", "run", "-n", "kidney-exchange", "/bin/bash", "-c"]
 
+# do all your magic from here
 # copy rest of the app
-COPY kidney_exchange .
+COPY kidney_exchange ./kidney_exchange
 
 # entry point - replace last part with start of the actual command
-ENTRYPOINT ["conda", "run", "-n", "kidney-exchange", "/bin/bash", "-c"]
+ENTRYPOINT ["conda", "run", "-n", "kidney-exchange", "/bin/bash", "-c", "gunicorn --bind 0.0.0.0:8080 kidney_exchange.web.app:app"]
---
+++ kidney_exchange/web/app.py
@@ -0,0 +1,42 @@
+import logging
+import os
+import sys
+
+from flask import Flask
+
+from kidney_exchange.database.db import db
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s] - %(levelname)s - %(module)s: %(message)s',
+                    stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
+def configure_db():
+    # TODO load configuration from file and override it with env - to discussion with team
+    user = os.environ.get("POSTGRES_USER")
+    password = os.environ.get("POSTGRES_PASSWORD")
+    url = os.environ.get("POSTGRES_URL")
+    po_db = os.environ.get("POSTGRES_DB")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] \
+        = f'postgresql+psycopg2://{user}:{password}@{url}/{po_db}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+
+with app.app_context():
+    configure_db()
+
+
+@app.route('/')
+def hello():
+    logger.info("Hello from the log!")
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
