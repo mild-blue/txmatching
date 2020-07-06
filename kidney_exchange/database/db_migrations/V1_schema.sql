6bfff66f19101af34d12023a1308cad3dcd98b67
Tomáš Kouřim <kourim@outlook.com>
2020-07-06 10:34:22 +0200

    Create more detailed DB schema

--- kidney_exchange/database/db_migrations/V1_schema.sql
+++ kidney_exchange/database/db_migrations/V1_schema.sql
@@ -27,7 +27,7 @@ CREATE TYPE BLOOD_TYPE AS ENUM (
 
 CREATE TABLE app_user (
     id        BIGSERIAL NOT NULL PRIMARY KEY,
-    username  VARCHAR   NOT NULL UNIQUE,
+    email     VARCHAR   NOT NULL UNIQUE, -- serves as username
     pass_hash VARCHAR   NOT NULL,
     role      VARCHAR   NOT NULL
 );
@@ -40,7 +40,8 @@ CREATE TABLE patient (
     blood            BLOOD_TYPE   NOT NULL,
     acceptable_blood VARCHAR      NOT NULL, -- list of BLOOD_TYPE
     typization       VARCHAR      NOT NULL, -- JSON
-    luminex          VARCHAR      NOT NULL  -- JSON
+    luminex          VARCHAR      NOT NULL, -- JSON
+    active           BOOLEAN      NOT NULL  -- assume some patients fall out of the set
 );
 
 CREATE TABLE patient_pair (
@@ -52,16 +53,17 @@ CREATE TABLE patient_pair (
 
 CREATE TABLE config (
     id         BIGSERIAL NOT NULL PRIMARY KEY,
-    parameters VARCHAR   NOT NULL UNIQUE, -- structured json
-    created    TIMESTAMP NOT NULL
+    parameters VARCHAR   NOT NULL UNIQUE,                 -- structured JSON
+    created    TIMESTAMP NOT NULL,
+    created_by BIGINT    NOT NULL REFERENCES app_user(id) -- this is also valid for pairing_result
 );
 
 CREATE TABLE pairing_result (
     id                   BIGSERIAL NOT NULL PRIMARY KEY,
     config_id            BIGINT    NOT NULL REFERENCES config(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    patient_ids          VARCHAR   NOT NULL, -- list of patient_id
+    patient_ids          VARCHAR   NOT NULL, -- list of patient_id (or JSON)
     calculated_matchings VARCHAR   NOT NULL, -- JSON
-    patient_matrix       VARCHAR   NOT NULL, -- matrix (list of lists) of computed compatibility indexes among patients
+    score_matrix         VARCHAR   NOT NULL, -- matrix (list of lists) of computed compatibility indexes among patients
     created              TIMESTAMP NOT NULL,
     valid                BOOLEAN   NOT NULL
 );
