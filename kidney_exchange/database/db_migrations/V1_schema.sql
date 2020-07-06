75ce0210d8bfd8cd9d2bd6b0cc2b5d1adfa7ea65
Tomáš Kouřim <kourim@outlook.com>
2020-07-06 20:41:05 +0200

    Specify types

--- kidney_exchange/database/db_migrations/V1_schema.sql
+++ kidney_exchange/database/db_migrations/V1_schema.sql
@@ -36,21 +36,22 @@ VALUES ('VIEWER'),
     ('ADMIN');
 
 CREATE TABLE app_user (
-    id        BIGSERIAL                NOT NULL,
-    email     VARCHAR                  NOT NULL UNIQUE, -- serves as username
-    pass_hash VARCHAR                  NOT NULL,
-    created   TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated   TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted   TIMESTAMP WITH TIME ZONE,
-    CONSTRAINT pkey_app_user_id PRIMARY KEY (id)
+    id        BIGSERIAL   NOT NULL,
+    email     VARCHAR     NOT NULL, -- serves as username
+    pass_hash VARCHAR     NOT NULL,
+    created   TIMESTAMPTZ NOT NULL,
+    updated   TIMESTAMPTZ NOT NULL,
+    deleted   TIMESTAMPTZ,
+    CONSTRAINT pkey_app_user_id PRIMARY KEY (id),
+    CONSTRAINT uq_app_user_email UNIQUE (email)
 );
 
 CREATE TABLE app_user_role (
-    app_user_id BIGINT                   NOT NULL,
-    role_id     BIGINT                   NOT NULL,
-    created     TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated     TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted     TIMESTAMP WITH TIME ZONE,
+    app_user_id BIGINT      NOT NULL,
+    role_id     BIGINT      NOT NULL,
+    created     TIMESTAMPTZ NOT NULL,
+    updated     TIMESTAMPTZ NOT NULL,
+    deleted     TIMESTAMPTZ,
     CONSTRAINT pkey_app_user_role PRIMARY KEY (app_user_id, role_id),
     CONSTRAINT uq_app_user_role UNIQUE (app_user_id, role_id),
     CONSTRAINT fkey_app_user_role FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -58,28 +59,28 @@ CREATE TABLE app_user_role (
 );
 
 CREATE TABLE patient (
-    id               BIGSERIAL                NOT NULL,
-    medical_id       VARCHAR                  NOT NULL,
-    country          COUNTRY                  NOT NULL,
-    patient_type     PATIENT_TYPE             NOT NULL,
-    blood            BLOOD_TYPE               NOT NULL,
-    acceptable_blood VARCHAR                  NOT NULL, -- list of BLOOD_TYPE
-    typization       VARCHAR                  NOT NULL, -- JSON
-    luminex          VARCHAR                  NOT NULL, -- JSON
-    active           BOOLEAN                  NOT NULL, -- assume some patients fall out of the set
-    created          TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated          TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted          TIMESTAMP WITH TIME ZONE,
+    id               BIGSERIAL    NOT NULL,
+    medical_id       VARCHAR      NOT NULL,
+    country          COUNTRY      NOT NULL,
+    patient_type     PATIENT_TYPE NOT NULL,
+    blood            BLOOD_TYPE   NOT NULL,
+    acceptable_blood VARCHAR      NOT NULL, -- list of BLOOD_TYPE
+    typization       JSONB        NOT NULL, -- JSON
+    luminex          JSONB        NOT NULL, -- JSON
+    active           BOOLEAN      NOT NULL, -- assume some patients fall out of the set
+    created          TIMESTAMPTZ  NOT NULL,
+    updated          TIMESTAMPTZ  NOT NULL,
+    deleted          TIMESTAMPTZ,
     CONSTRAINT pkey_patient_id PRIMARY KEY (id)
 );
 
 CREATE TABLE patient_pair (
-    id           BIGSERIAL                NOT NULL,
-    recipient_id BIGINT                   NOT NULL,
-    donor_id     BIGINT                   NOT NULL,
-    created      TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated      TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted      TIMESTAMP WITH TIME ZONE,
+    id           BIGSERIAL   NOT NULL,
+    recipient_id BIGINT      NOT NULL,
+    donor_id     BIGINT      NOT NULL,
+    created      TIMESTAMPTZ NOT NULL,
+    updated      TIMESTAMPTZ NOT NULL,
+    deleted      TIMESTAMPTZ,
     CONSTRAINT pkey_patient_pair_id PRIMARY KEY (id),
     CONSTRAINT fkey_patient_pair_recipient_id_patient_id FOREIGN KEY (recipient_id) REFERENCES patient(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fkey_patient_pair_donor_id_patient_id FOREIGN KEY (donor_id) REFERENCES patient(id) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -87,27 +88,27 @@ CREATE TABLE patient_pair (
 );
 
 CREATE TABLE config (
-    id         BIGSERIAL                NOT NULL,
-    parameters VARCHAR                  NOT NULL,                                                  -- structured JSON
-    created_by BIGINT                   NOT NULL,
-    created    TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated    TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted    TIMESTAMP WITH TIME ZONE,
+    id         BIGSERIAL   NOT NULL,
+    parameters JSONB       NOT NULL,                                                               -- structured JSON
+    created_by BIGINT      NOT NULL,
+    created    TIMESTAMPTZ NOT NULL,
+    updated    TIMESTAMPTZ NOT NULL,
+    deleted    TIMESTAMPTZ,
     CONSTRAINT pkey_config_id PRIMARY KEY (id),
     CONSTRAINT uq_config_parameters UNIQUE (parameters),
     CONSTRAINT fkey_config_created_by_app_user_id FOREIGN KEY (created_by) REFERENCES app_user(id) -- this is also valid for pairing_result
 );
 
 CREATE TABLE pairing_result (
-    id                   BIGSERIAL                NOT NULL,
-    config_id            BIGINT                   NOT NULL,
-    patient_ids          VARCHAR                  NOT NULL, -- list of patient_id (or JSON)
-    calculated_matchings VARCHAR                  NOT NULL, -- JSON
-    score_matrix         VARCHAR                  NOT NULL, -- matrix (list of lists) of computed compatibility indexes among patients
-    valid                BOOLEAN                  NOT NULL,
-    created              TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated              TIMESTAMP WITH TIME ZONE NOT NULL,
-    deleted              TIMESTAMP WITH TIME ZONE,
+    id                   BIGSERIAL   NOT NULL,
+    config_id            BIGINT      NOT NULL,
+    patient_ids          JSONB       NOT NULL, -- list of patient_id (or JSON)
+    calculated_matchings JSONB       NOT NULL, -- JSON
+    score_matrix         VARCHAR     NOT NULL, -- matrix (list of lists) of computed compatibility indexes among patients
+    valid                BOOLEAN     NOT NULL,
+    created              TIMESTAMPTZ NOT NULL,
+    updated              TIMESTAMPTZ NOT NULL,
+    deleted              TIMESTAMPTZ,
     CONSTRAINT pkey_pairing_result_id PRIMARY KEY (id),
     CONSTRAINT fkey_pairing_result_config_id_config_id FOREIGN KEY (config_id) REFERENCES config(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
