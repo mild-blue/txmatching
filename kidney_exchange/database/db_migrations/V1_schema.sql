5392e3f1d267bc25a5776be9f381e5582ae099d8
Tomáš Kouřim <kourim@outlook.com>
2020-07-08 22:59:00 +0200

    Add link to Trello TODOs

--- kidney_exchange/database/db_migrations/V1_schema.sql
+++ kidney_exchange/database/db_migrations/V1_schema.sql
@@ -194,5 +194,5 @@ $$
 $$
 LANGUAGE plpgsql;
 
--- TODO add indexes (probably only when actually needed)
--- TODO add (before|instead) triggers for delete
+-- TODO add indexes (probably only when actually needed) (https://trello.com/c/H1gtKSao/89-add-indexes-to-db-probably-only-when-actually-needed)
+-- TODO add (before|instead) triggers for delete (https://trello.com/c/MfPrpOp1/90-add-beforeinstead-triggers-for-delete-to-db)
