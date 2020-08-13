d7814509afcb27188803ecabce36ed93187ef62a
anastasia <anastasia.surikova@akka.eu>
2020-08-13 18:57:15 +0200

    works but color are not pretty

--- kidney_exchange/web/static/js/app.js
+++ kidney_exchange/web/static/js/app.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     if(window.innerWidth > 1200) {
         scrollWatcher();
     }
+
+    initColors();
 })
 
 function scrollWatcher() {
@@ -23,18 +25,31 @@ function scrollWatcher() {
 }
 
 // Gradient generator
-const rounds = document.querySelectorAll('[data-js-selector="set-score-gradient"]');
 
-for(let round of rounds) {
-    const score = round.getAttribute('data-score');
-    const colorObject = bezierInterpolation(score);
-    const colorString = `rgb(${colorObject.r}, ${colorObject.g}, ${colorObject.b})`;
-    round.setAttribute('style', `border-right: 20px solid ${colorString}`);
+function initColors() {
+    const colorMap = new Map();
+    const maxScore = 27;
+    const increment = 1.0 / (maxScore - 1);
+
+    for (let i = 0; i <= maxScore; i++) {
+        const colorObject = bezierInterpolation(increment * i);
+        colorMap.set(i, `rgb(${colorObject.r}, ${colorObject.g}, ${colorObject.b})`);
+    }
+
+    const rounds = document.querySelectorAll('[data-js-selector="set-score-gradient"]');
+
+    for(let round of rounds) {
+        const score = Number(round.getAttribute('data-score'));
+        const color = colorMap.get(score);
+        if(color) {
+            round.setAttribute('style', `border-right: 20px solid ${color}`);
+        }
+    }
 }
 
 function bezierInterpolation(x) {
-    const firstColor = {r: 226, g: 0, b: 26};
-    const secondColor = {r: 226, g: 0, b: 26}; // todo
+    const firstColor = {r: 226, g: 0, b: 26}; // minimum -- red
+    const secondColor = {r: 0, g: 128, b: 0}; // maximum -- green
     const y = 1 - x;
 
     const vR = y * firstColor.r + x * secondColor.r;
