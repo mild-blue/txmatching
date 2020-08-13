a09fc11bfb40765921f609db94c5089f629e8914
anastasia <anastasia.surikova@akka.eu>
2020-08-13 18:43:06 +0200

    gradient generator

--- kidney_exchange/web/static/js/app.js
+++ kidney_exchange/web/static/js/app.js
@@ -21,3 +21,25 @@ function scrollWatcher() {
         }
     })
 }
+
+// Gradient generator
+const rounds = document.querySelectorAll('[data-js-selector="set-score-gradient"]');
+
+for(let round of rounds) {
+    const score = round.getAttribute('data-score');
+    const colorObject = bezierInterpolation(score);
+    const colorString = `rgb(${colorObject.r}, ${colorObject.g}, ${colorObject.b})`;
+    round.setAttribute('style', `border-right: 20px solid ${colorString}`);
+}
+
+function bezierInterpolation(x) {
+    const firstColor = {r: 226, g: 0, b: 26};
+    const secondColor = {r: 226, g: 0, b: 26}; // todo
+    const y = 1 - x;
+
+    const vR = y * firstColor.r + x * secondColor.r;
+    const vG = y * firstColor.g + x * secondColor.g;
+    const vB = y * firstColor.b + x * secondColor.b;
+
+    return { r: Math.trunc(vR), g: Math.trunc(vG), b: Math.trunc(vB) };
+}
