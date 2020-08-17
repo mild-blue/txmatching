6696d1b1648c984708c2cfd58a91228e39371788
anastasia <anastasia.surikova@akka.eu>
2020-08-17 17:36:27 +0200

    styles only gradient

--- kidney_exchange/web/static/js/app.js
+++ kidney_exchange/web/static/js/app.js
@@ -21,32 +21,3 @@ function scrollWatcher() {
         }
     })
 }
-
-// Gradient generator
-
-const rounds = document.querySelectorAll('[data-js-selector="set-score-gradient"]');
-for(let round of rounds) {
-    const score = Number(round.getAttribute('data-score'));
-    if(score >= 0 && score <= 1) {
-        const color = getGradientColor(score);
-        if(color) {
-            round.setAttribute('style', `border-right: 20px solid ${color}`);
-        }
-    }
-}
-
-function getGradientColor(score) {
-    // generating colors in this gradient range: https://uigradients.com/#KyooPal
-    const startColor = {r: 221, g: 62, b: 84}; // score = 0 -> red
-    const endColor = {r: 107, g: 229, b: 113}; // score = 1 -> green
-
-    let diffRed = endColor.r - startColor.r;
-    let diffGreen = endColor.g - startColor.g;
-    let diffBlue = endColor.b - startColor.b;
-
-    diffRed = (diffRed * score) + startColor.r;
-    diffGreen = (diffGreen * score) + startColor.g;
-    diffBlue = (diffBlue * score) + startColor.b;
-
-    return `rgb(${diffRed}, ${diffGreen}, ${diffBlue})`;
-}
