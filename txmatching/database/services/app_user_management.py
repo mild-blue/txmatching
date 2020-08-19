a57a4ec070dd0acb826c6fa10cde4786497bf3e5
Lukas Forst <lukas@forst.pw>
2020-08-19 11:13:56 +0200

    do not use one, use first on email, because there's constraint

--- txmatching/database/services/app_user_management.py
+++ txmatching/database/services/app_user_management.py
@@ -1,8 +1,6 @@
 import logging
 from typing import Optional
 
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
-
 from txmatching.database.db import db
 from txmatching.database.sql_alchemy_schema import AppUser
 
@@ -14,13 +12,7 @@ def get_all_app_users():
 
 
 def get_app_user_by_email(email: str) -> Optional[AppUser]:
-    try:
-        return AppUser.query.filter(AppUser.email == email).one()
-    except MultipleResultsFound:
-        logger.error("Multiple users with the same email found.")
-        return None
-    except NoResultFound:
-        return None
+    return AppUser.query.filter(AppUser.email == email).first()
 
 
 def persist_user(user: AppUser):
