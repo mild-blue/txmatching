dc2ab61af37701636c90b20a0da5a33f8d4fa536
honza <jan.kubant@blindspot.ai>
2020-08-19 17:02:30 +0200

    delete old fe and old apis

--- kidney_exchange/web/api/patient_api.py
+++ kidney_exchange/web/api/patient_api.py
@@ -1,16 +1,14 @@
 import dataclasses
 import logging
 
-from flask import Blueprint, jsonify
+from flask import jsonify
 from flask_restx import Resource
 
 from kidney_exchange.data_transfer_objects.patients.patient_swagger import PATIENT_MODEL
 from kidney_exchange.database.services.patient_service import get_all_patients
-from kidney_exchange.web.api.namespaces import patient_api, PATIENT_NAMESPACE
+from kidney_exchange.web.api.namespaces import patient_api
 
 logger = logging.getLogger(__name__)
-
-patient_blueprint = Blueprint(PATIENT_NAMESPACE, __name__)
 
 
 @patient_api.route('/', methods=['GET'])
