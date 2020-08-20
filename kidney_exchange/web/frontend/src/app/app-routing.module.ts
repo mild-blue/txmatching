2bd87face365bbcdfb2edd4dc15d3c116fb36ae7
anastasia <anastasia.surikova@akka.eu>
2020-08-20 12:53:55 +0200

    guard, components, routes, env added

--- kidney_exchange/web/frontend/src/app/app-routing.module.ts
+++ kidney_exchange/web/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '@app/pages/home/home.component';
+import { LoginComponent } from '@app/pages/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
