78548c3698d276a24b4bba35a14319c4102d5209
anastasia <anastasia.surikova@akka.eu>
2020-08-20 15:17:48 +0200

    interceptors and guard content

--- kidney_exchange/web/frontend/src/app/interceptors/error/error.interceptor.ts
+++ kidney_exchange/web/frontend/src/app/interceptors/error/error.interceptor.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { AuthService } from '@app/services/auth/auth.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(private _authService: AuthService) {
   }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      if (err.status === 401) {
+        // auto logout if 401 response returned from api
+        this._authService.logout();
+        location.reload(true);
+      }
+
+      const error = err.error.message || err.statusText;
+      return throwError(error);
+    }));
   }
 }
