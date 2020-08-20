f22b89f7a2ea895752bde3e02450eb504f8b65df
anastasia <anastasia.surikova@akka.eu>
2020-08-20 15:17:48 +0200

    interceptors and guard content

--- kidney_exchange/web/frontend/src/app/helpers/auth/auth.guard.ts
+++ kidney_exchange/web/frontend/src/app/helpers/auth/auth.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '@app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private _router: Router,
+              private _authService: AuthService) {
   }
 
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const currentUser = this._authService.currentUserValue;
+    if (currentUser) {
+      return true;
+    }
+
+    this._router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 }
--- kidney_exchange/web/frontend/src/app/interceptors/auth/auth-interceptor.interceptor.ts
+++ kidney_exchange/web/frontend/src/app/interceptors/auth/auth-interceptor.interceptor.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '@app/services/auth/auth.service';
 
 @Injectable()
 export class AuthInterceptorInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(private _authService: AuthService) {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // add authorization header with basic auth credentials if available
+    const currentUser = this._authService.currentUserValue;
+    if (currentUser && currentUser.authData) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Basic ${currentUser.authData}`
+        }
+      });
+    }
+
     return next.handle(request);
   }
 }
