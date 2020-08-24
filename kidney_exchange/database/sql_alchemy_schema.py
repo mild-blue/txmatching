af17e27217b5029fea578fa92157a9d84d814c9f
honza <jan.kubant@blindspot.ai>
2020-08-24 07:49:25 +0200

    Add comments explaning why some things were done in SQL alchemy model

--- kidney_exchange/database/sql_alchemy_schema.py
+++ kidney_exchange/database/sql_alchemy_schema.py
@@ -11,10 +11,13 @@ from kidney_exchange.patients.patient import PatientType
 class ConfigModel(db.Model):
     __tablename__ = 'config'
     __table_args__ = {'extend_existing': True}
-
-    id = db.Column(Integer, primary_key=True, autoincrement=True)
+    # Here and below I am using Integer instead of BigInt because it seems that there is a bug and BigInteger is not
+    # transfered to BigSerial with autoincrement True, but to BigInt only.
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     parameters = db.Column(db.JSON, unique=False, nullable=False)
-    created_by = db.Column(db.BigInteger, unique=False, nullable=False)
+    created_by = db.Column(db.Integer, unique=False, nullable=False)
+    # created at and updated at is not handled by triggers as then am not sure how tests would work, as triggers
+    # seem to be specific as per db and I do not think its worth the effort as this simple approach works fine
     created_at = db.Column(db.DateTime(timezone=True), unique=False, nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime(timezone=True), nullable=True)
@@ -24,8 +27,8 @@ class PairingResultModel(db.Model):
     __tablename__ = 'pairing_result'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    config_id = db.Column(db.BigInteger, unique=False, nullable=False)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    config_id = db.Column(db.Integer, unique=False, nullable=False)
     calculated_matchings = db.Column(db.JSON, unique=False, nullable=False)
     score_matrix = db.Column(db.JSON, unique=False, nullable=False)
     valid = db.Column(db.BOOLEAN, unique=False, nullable=False)
@@ -39,9 +42,9 @@ class PairingResultPatientModel(db.Model):
     __tablename__ = 'pairing_result_patient'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pairing_result_id = db.Column(db.BigInteger, ForeignKey('pairing_result.id'), unique=False, nullable=False)
-    patient_id = db.Column(db.BigInteger, ForeignKey('patient.id'), unique=False, nullable=False)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    pairing_result_id = db.Column(db.Integer, ForeignKey('pairing_result.id'), unique=False, nullable=False)
+    patient_id = db.Column(db.Integer, ForeignKey('patient.id'), unique=False, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), unique=False, nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime(timezone=True), nullable=True)
@@ -51,7 +54,7 @@ class PatientModel(db.Model):
     __tablename__ = 'patient'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     medical_id = db.Column(db.TEXT, unique=False, nullable=False)
     country = db.Column(db.TEXT, unique=False, nullable=False)
     patient_type = db.Column(db.Enum(PatientType), unique=False, nullable=False)
@@ -71,8 +74,8 @@ class PatientAcceptableBloodModel(db.Model):
     __tablename__ = 'patient_acceptable_blood'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    patient_id = db.Column(db.BigInteger, ForeignKey('patient.id'), unique=False, nullable=False)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    patient_id = db.Column(db.Integer, ForeignKey('patient.id'), unique=False, nullable=False)
     blood_type = db.Column(db.TEXT, unique=False, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), unique=False, nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
@@ -83,9 +86,11 @@ class PatientPairModel(db.Model):
     __tablename__ = 'patient_pair'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    recipient_id = db.Column(db.BigInteger, ForeignKey('patient.id'), unique=False, nullable=False)
-    donor_id = db.Column(db.BigInteger, unique=False, nullable=False)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    recipient_id = db.Column(db.Integer, ForeignKey('patient.id'), unique=False, nullable=False)
+    # Add also Foreign key to donor ID below, as it will be two foreign keys to one table its a bit tricky to do
+    # but it should be possible, just not done for the time being. https://trello.com/c/KtNzIBJa
+    donor_id = db.Column(db.Integer, unique=False, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), unique=False, nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
     deleted_at = db.Column(db.DateTime(timezone=True), nullable=True)
@@ -95,7 +100,7 @@ class AppUser(db.Model):
     __tablename__ = 'app_user'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = db.Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     email = db.Column(db.TEXT, unique=True, nullable=False)
     pass_hash = db.Column(db.TEXT, unique=False, nullable=False)
     role = db.Column(db.TEXT, unique=False, nullable=False)
