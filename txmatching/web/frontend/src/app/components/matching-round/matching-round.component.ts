2e2b2d0e84c2b1d53dd24a52a05aabeb27fe0154
Anastasia <surikova@dalten.cz>
2020-08-30 20:27:07 +0200

    round index due to donor type

--- txmatching/web/frontend/src/app/components/matching-round/matching-round.component.ts
+++ txmatching/web/frontend/src/app/components/matching-round/matching-round.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Round, Transplant } from '@app/model/Matching';
-import { compatibleBloodGroups, Patient } from '@app/model/Patient';
+import { compatibleBloodGroups, Patient, PatientType } from '@app/model/Patient';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { AppConfiguration } from '@app/model/Configuration';
 
@@ -22,7 +22,24 @@ export class MatchingRoundComponent implements OnInit {
   }
 
   get roundIndex(): string {
-    return '';
+    const roundIndex = `${this.index}`;
+    if (!this.round || !this.round.transplants.length) {
+      return roundIndex;
+    }
+
+    const firstTransplant = this.round.transplants[0];
+    const donor = this.donor(firstTransplant);
+
+    if (donor) {
+      if (donor.patient_type === PatientType.BRIDGING_DONOR.valueOf()) {
+        return `${roundIndex}B`;
+      }
+      if (donor.patient_type === PatientType.ALTRUIST.valueOf()) {
+        return `${roundIndex}A`;
+      }
+    }
+
+    return roundIndex;
   }
 
   ngOnInit(): void {
