33ad8d3dad7ad4fcc7b8e74ddf513f6dff3b1ad5
Lukas Forst <lukas@forst.pw>
2020-08-31 10:10:24 +0200

    add back cache, explicitly saving default conf, possible lint fix

--- txmatching/database/services/config_service.py
+++ txmatching/database/services/config_service.py
@@ -38,6 +38,8 @@ def save_configuration_as_current(configuration: Configuration) -> int:
     if maybe_config is not None:
         db.session.delete(maybe_config)
     config_model = _configuration_to_config_model(configuration)
+    # explicitly saving as 0 -> default configuration
+    config_model.id = 0
     db.session.add(config_model)
     db.session.commit()
     return config_model.id
