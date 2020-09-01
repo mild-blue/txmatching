dd09fa6147fdd0e4187743229662b7cd8ea05c77
anastasia <anastasia.surikova@akka.eu>
2020-09-01 17:42:40 +0200

    take origin instead of full href

--- txmatching/web/frontend/src/environments/environment.prod.ts
+++ txmatching/web/frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ const apiVersion = 'v1';
 
 export const environment = {
   production: true,
-  apiUrl: `${window.location.href}${apiVersion}`
+  apiUrl: `${window.location.origin}/${apiVersion}`
 };
