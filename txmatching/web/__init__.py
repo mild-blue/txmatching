892e9d4c167b25ab42b7fd65bbb6816bf447acfe
Lukas Forst <lukas@forst.pw>
2020-09-02 22:04:57 +0200

    allow cors only for really specific origin

--- txmatching/web/__init__.py
+++ txmatching/web/__init__.py
@@ -90,8 +90,9 @@ def create_app():
                 'http://localhost:4200',  # localhost development
                 'https://127.0.0.1:9090'  # proxy on staging, support for swagger
             }
-            if request.headers.get('origin') in allowed_origins:
-                response.headers.add('Access-Control-Allow-Origin', '*')
+            origin = request.headers.get('origin')
+            if origin in allowed_origins:
+                response.headers.add('Access-Control-Allow-Origin', origin)
                 response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
             return response
 
