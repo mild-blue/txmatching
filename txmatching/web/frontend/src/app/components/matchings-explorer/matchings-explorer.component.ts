7bc09f10dba877db75625a52270f9bb990274f0b
anastasia <anastasia.surikova@akka.eu>
2020-09-02 16:44:48 +0200

    sexy style adjusting when scrolling

--- txmatching/web/frontend/src/app/components/matchings-explorer/matchings-explorer.component.ts
+++ txmatching/web/frontend/src/app/components/matchings-explorer/matchings-explorer.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { MatchingView } from '@app/model/Matching';
 import { Patient } from '@app/model/Patient';
 import { AppConfiguration } from '@app/model/Configuration';
@@ -8,7 +8,7 @@ import { AppConfiguration } from '@app/model/Configuration';
   templateUrl: './matchings-explorer.component.html',
   styleUrls: ['./matchings-explorer.component.scss']
 })
-export class MatchingsExplorerComponent implements OnInit {
+export class MatchingsExplorerComponent implements OnInit, AfterViewInit {
 
   @ViewChild('list') list?: ElementRef;
   @ViewChild('detail') detail?: ElementRef;
@@ -18,12 +18,18 @@ export class MatchingsExplorerComponent implements OnInit {
   @Input() configuration?: AppConfiguration;
 
   public activeMatching?: MatchingView;
+  public activeAlignedTop: boolean = false;
+  public activeAlignedBottom: boolean = false;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.setActive(this.matchings.length ? this.matchings[0] : undefined);
+  }
+
+  ngAfterViewInit(): void {
+    this._initAdjustingStylesOnScroll(this.list, this.detail);
   }
 
   public setActive(matching: MatchingView | undefined): void {
@@ -50,5 +56,34 @@ export class MatchingsExplorerComponent implements OnInit {
     if (this.detail) {
       this.detail.nativeElement.scrollTop = 0;
     }
+  }
+
+  private _initAdjustingStylesOnScroll(list?: ElementRef, detail?: ElementRef): void {
+    if (!list || !detail) {
+      return;
+    }
+
+    const tolerance = 40;
+    const listElement = list.nativeElement;
+    const detailHeight = detail.nativeElement.offsetHeight;
+    const detailRectangle = detail.nativeElement.getBoundingClientRect();
+
+    const detailTop = detailRectangle.top + tolerance;
+    const detailBottom = detailRectangle.top + detailHeight - tolerance;
+
+    listElement.addEventListener('scroll', (e) => {
+      const active = listElement.querySelector('.active');
+      if (!active) {
+        return;
+      }
+      const activeHeight = active.offsetHeight;
+      const activeRectangle = active.getBoundingClientRect();
+
+      const activeTop = activeRectangle.top - tolerance;
+      const activeBottom = activeRectangle.top + activeHeight + tolerance;
+
+      this.activeAlignedTop = activeTop <= detailTop;
+      this.activeAlignedBottom = activeBottom >= detailBottom;
+    });
   }
 }
