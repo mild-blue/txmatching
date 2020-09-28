1de839b3c04bf43eab478389fef5a1eba610634c
Tomáš Kouřim <kourim@outlook.com>
2020-09-28 14:20:08 +0200

    Rename classes to CamelCase 2 Rename Donor and Recipient according to review

--- txmatching/data_transfer_objects/patients/patient_swagger.py
+++ txmatching/data_transfer_objects/patients/patient_swagger.py
@@ -6,31 +6,31 @@ from txmatching.web.api.namespaces import patient_api
 
 BLOOD_TYPES = ['A', 'B', 'AB', '0']
 
-HLA_ANTIBODY = patient_api.model('HlaAntibody', {
+HLAAntibody = patient_api.model('HlaAntibody', {
     'raw_code': fields.String(required=True),
     'mfi': fields.Integer(required=True),
     'cutoff': fields.String(required=True),
     'code': fields.String(required=False)
 })
 
-HLA_TYPE = patient_api.model('HlaType', {
+HLAType = patient_api.model('HlaType', {
     'code': fields.List(required=True, cls_or_instance=fields.String),
     'raw_code': fields.String(required=True),
 })
 
-HLA_TYPING = patient_api.model('HlaTyping', {
-    'hla_types_list': fields.List(required=True, cls_or_instance=fields.Nested(HLA_TYPE)),
+HLATyping = patient_api.model('HlaTyping', {
+    'hla_types_list': fields.List(required=True, cls_or_instance=fields.Nested(HLAType)),
     'codes': fields.List(required=True, cls_or_instance=fields.String),
 })
 
-HLA_ANTIBODIES = patient_api.model('HlaAntibodies', {
-    'hla_antibodies_list': fields.List(required=True, cls_or_instance=fields.Nested(HLA_ANTIBODY)),
+HLAAntibodies = patient_api.model('HlaAntibodies', {
+    'hla_antibodies_list': fields.List(required=True, cls_or_instance=fields.Nested(HLAAntibody)),
     'hla_codes_over_cutoff': fields.List(required=True, cls_or_instance=fields.String),
 })
 
-PATIENT_PARAMETERS_MODEL = patient_api.model('PatientParameters', {
+PatientParametersModel = patient_api.model('PatientParameters', {
     'blood_group': fields.String(required=False, enum=BLOOD_TYPES),
-    'hla_typing': fields.List(required=False, cls_or_instance=fields.Nested(HLA_TYPING)),
+    'hla_typing': fields.List(required=False, cls_or_instance=fields.Nested(HLATyping)),
     'country_code': fields.String(required=False),
     'sex': fields.String(required=False, enum=[sex.name for sex in Sex]),
     'height': fields.Integer(required=False),
@@ -38,74 +38,74 @@ PATIENT_PARAMETERS_MODEL = patient_api.model('PatientParameters', {
     'YOB': fields.Integer(required=False),
 })
 
-RECIPIENT_REQUIREMENTS = patient_api.model('RecipientRequirements', {
+RecipientRequirements = patient_api.model('RecipientRequirements', {
     'require_better_match_in_compatibility_index': fields.Boolean(required=True),
     'require_better_match_in_compatibility_index_or_blood_group': fields.Boolean(required=True),
     'require_compatible_blood_group': fields.Boolean(required=True)
 })
 
-DONOR_MODEL = patient_api.model('Donor', {
+DonorModel = patient_api.model('Donor model', {
     'db_id': fields.Integer(required=True, description='Database id of the patient'),
     'medical_id': fields.String(required=True, description='Medical id of the patient'),
-    'parameters': fields.Nested(required=True, model=PATIENT_PARAMETERS_MODEL),
+    'parameters': fields.Nested(required=True, model=PatientParametersModel),
     'donor_type': fields.String(required=True, enum=[donor_type.name for donor_type in DonorType]),
     'related_recipient_db_id': fields.Integer(required=False, description='Database id of the related recipient'),
 })
 
-RECIPIENT_MODEL = patient_api.model('Recipient', {
+RecipientModel = patient_api.model('Recipient model', {
     'db_id': fields.Integer(required=True, description='Database id of the patient'),
     'acceptable_blood_groups': fields.List(required=False, cls_or_instance=fields.String(enum=BLOOD_TYPES)),
     'medical_id': fields.String(required=True, description='Medical id of the patient'),
-    'parameters': fields.Nested(required=True, model=PATIENT_PARAMETERS_MODEL),
-    'hla_antibodies': fields.Nested(required=True, model=HLA_ANTIBODIES),
+    'parameters': fields.Nested(required=True, model=PatientParametersModel),
+    'hla_antibodies': fields.Nested(required=True, model=HLAAntibodies),
     'related_donor_db_id': fields.Integer(required=True, description='Database id of the related donor'),
-    'recipient_requirements': fields.Nested(RECIPIENT_REQUIREMENTS),
+    'recipient_requirements': fields.Nested(RecipientRequirements),
     'waiting_since': fields.DateTime(required=False),
     'previous_transplants': fields.Integer(required=False),
 })
 
-PATIENTS_MODEL = patient_api.model('Patients', {
-    'recipients': fields.List(required=False, cls_or_instance=fields.Nested(RECIPIENT_MODEL)),
-    'donors': fields.List(required=False, cls_or_instance=fields.Nested(DONOR_MODEL))
+PatientsModel = patient_api.model('Patients', {
+    'recipients': fields.List(required=False, cls_or_instance=fields.Nested(RecipientModel)),
+    'donors': fields.List(required=False, cls_or_instance=fields.Nested(DonorModel))
 })
 
-HLA_ANTIBODY_TO_UPDATE = patient_api.model('HlaAntibodyToUpdate', {
+HLAAntibodyToUpdate = patient_api.model('HlaAntibodyToUpdate', {
     'raw_code': fields.String(required=True),
     'mfi': fields.Integer(required=True),
 })
 
-HLA_TYPE_TO_UPDATE = patient_api.model('HlaTypeToUpdate', {
+HLATypeToUpdate = patient_api.model('HlaTypeToUpdate', {
     'raw_code': fields.String(required=True),
 })
 
-HLA_TYPING_TO_UPDATE = patient_api.model('HlaTyping', {
-    'hla_types_list': fields.List(required=True, cls_or_instance=fields.Nested(HLA_TYPE_TO_UPDATE)),
+HLATypingToUpdate = patient_api.model('HlaTyping', {
+    'hla_types_list': fields.List(required=True, cls_or_instance=fields.Nested(HLATypeToUpdate)),
 })
 
-HLA_ANTIBODIES_TO_UPDATE = patient_api.model('HlaAntibodies', {
-    'hla_antibodies_list': fields.List(required=True, cls_or_instance=fields.Nested(HLA_ANTIBODY_TO_UPDATE)),
+HLAAntibodiesToUpdate = patient_api.model('HlaAntibodies', {
+    'hla_antibodies_list': fields.List(required=True, cls_or_instance=fields.Nested(HLAAntibodyToUpdate)),
 })
 
-RECIPIENT_MODEL_TO_UPDATE = patient_api.model('RecipientModelToUpdate', {
+RecipientModelToUpdate = patient_api.model('RecipientModelToUpdate', {
     'db_id': fields.Integer(required=True, description='Database id of the patient'),
     'acceptable_blood_groups': fields.List(required=False, cls_or_instance=fields.String(enum=BLOOD_TYPES),
                                            description='Provide full list of all the acceptable blood groups of the '
                                                        'patient, not just the change set'),
-    'hla_typing': fields.Nested(HLA_TYPING_TO_UPDATE, required=False,
+    'hla_typing': fields.Nested(HLATypingToUpdate, required=False,
                                 description='Provide full list of all the HLA types of the patient, not just '
                                             'the change set'),
-    'hla_antibodies': fields.Nested(HLA_ANTIBODIES_TO_UPDATE, required=False,
+    'hla_antibodies': fields.Nested(HLAAntibodiesToUpdate, required=False,
                                     description='Provide full list of all the HLA antibodies of the patient, not just '
                                                 'the change set'),
-    'recipient_requirements': fields.Nested(RECIPIENT_REQUIREMENTS, required=False,
+    'recipient_requirements': fields.Nested(RecipientRequirements, required=False,
                                             description='Provide the whole recipients requirements object, it will be'
                                                         ' overwritten'),
     'cutoff': fields.Integer(required=False)
 })
 
-DONOR_MODEL_TO_UPDATE = patient_api.model('DonorModelToUpdate', {
+DonorModelToUpdate = patient_api.model('DonorModelToUpdate', {
     'db_id': fields.Integer(required=True, description='Database id of the patient'),
-    'hla_typing': fields.Nested(HLA_TYPING_TO_UPDATE, required=False,
+    'hla_typing': fields.Nested(HLATypingToUpdate, required=False,
                                 description='Provide full list of all the HLA types of the patient, not just '
                                             'the change set'),
 })
