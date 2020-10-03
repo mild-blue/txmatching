4ecf828066a606916980e3425cc356476dc3eb92
anastasia <anastasia.surikova@akka.eu>
2020-10-03 23:40:50 +0200

    service and output event

--- txmatching/web/frontend/src/app/components/header/header.component.ts
+++ txmatching/web/frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '@app/model/User';
 import { faQuestionCircle, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '@app/services/auth/auth.service';
@@ -13,6 +13,8 @@ export class HeaderComponent {
   private _openedDropdownId: string = '';
 
   @Input() user?: User;
+  @Input() showDownload: boolean = false;
+  @Output() downloadAction: EventEmitter<void> = new EventEmitter<void>();
 
   public userIcon = faUserAlt;
   public infoIcon = faQuestionCircle;
@@ -44,5 +46,9 @@ export class HeaderComponent {
 
   public logOut(): void {
     this._authService.logout();
+  }
+
+  public handleDownloadClick(): void {
+    this.downloadAction.emit();
   }
 }
---
+++ txmatching/web/frontend/src/app/services/report/report.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { first } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  constructor(private _http: HttpClient) {
+  }
+
+  public async downloadReport(matchingId: number): Promise<unknown> {
+    return this._http.get<unknown>(
+      `${environment.apiUrl}/report/${matchingId}`
+    ).pipe(first()).toPromise();
+  }
+}
