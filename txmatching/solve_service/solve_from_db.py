0a57a904f1bbf4c3e749b38f66aefd3e67b006e6
Jan Kubant <jan.kubant@blindspot.ai>
2020-10-05 12:28:43 +0200

    Remove unused import

--- txmatching/solve_service/solve_from_db.py
+++ txmatching/solve_service/solve_from_db.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Iterable, Optional
+from typing import Iterable
 
 from txmatching.configuration.configuration import Configuration
 from txmatching.database.db import db
