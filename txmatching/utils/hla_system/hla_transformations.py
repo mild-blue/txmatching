dc161aaad16462c64d8f76017fefd849fafb0c27
Jan Kubant <jan.kubant@blindspot.ai>
2020-10-07 10:58:36 +0200

    move no value enum under the correct comment

--- txmatching/utils/hla_system/hla_transformations.py
+++ txmatching/utils/hla_system/hla_transformations.py
@@ -32,9 +32,10 @@ class HlaCodeProcessingResultDetail(str, Enum):
     # still returning value
     SUCCESSFULLY_PARSED = 'Code successfully parsed without anything unexpected'
     UNEXPECTED_SPLIT_RES_CODE = 'Unknown HLA code, seems to be in split resolution'
+
+    # returning no value
     MULTIPLE_SPLITS_FOUND = 'Multiple splits were found, unable to choose the right one.' \
                             ' Immunologists shall be contacted'
-    # returning no value
     UNKNOWN_TRANSFORMATION_TO_SPLIT = 'Unable to transform high resolution HLA code. Its transformation to split' \
                                       ' code is unknown. Immunologists shall be contacted'
     UNPARSABLE_HLA_CODE = 'Completely Unexpected HLA code'
