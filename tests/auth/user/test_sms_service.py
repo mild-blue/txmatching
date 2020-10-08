036d6b3127d795376fff1ad57ffa2251b52ae987
Lukas Forst <lukas@forst.pw>
2020-10-08 17:20:29 +0200

    add failing test

--- tests/auth/user/test_sms_service.py
+++ tests/auth/user/test_sms_service.py
@@ -2,6 +2,7 @@ from unittest import TestCase, mock
 
 import responses
 
+from txmatching.auth.exceptions import CouldNotSendOtpUsingSmsServiceException
 from txmatching.auth.user.sms_service import _send_otp_ikem
 
 
@@ -29,7 +30,32 @@ class TestSmsService(TestCase):
         responses.add(
             'GET',
             url=expected_url,
-            match_querystring=True
+            match_querystring=True,
+            status=200
         )
 
         _send_otp_ikem(recipient_phone=phone, message_body=body, app_config=app_config)
+
+    @responses.activate
+    def test__send_sms_should_fail(self):
+        app_config = mock.MagicMock()
+        app_config.sms_service_url = 'https://some.domain/isg/index.php'
+        app_config.sms_service_sender = 'sender1'
+        app_config.sms_service_login = 'test'
+        app_config.sms_service_password = 'test123'
+
+        phone = '+420602613357'
+        body = 'Test'
+
+        responses.add(
+            'GET',
+            url=app_config.sms_service_url,
+            match_querystring=False,
+            status=500,
+            json='{\"error\":\"some error\"}'
+        )
+
+        self.assertRaises(
+            CouldNotSendOtpUsingSmsServiceException,
+            lambda: _send_otp_ikem(recipient_phone=phone, message_body=body, app_config=app_config)
+        )
