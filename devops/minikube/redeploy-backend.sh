44837ae37ff49abfcec51d6c71018542c8170b38
Marek Polak <marazt@gmail.com>
2020-10-10 14:09:14 +0200

    Update of the deployment script to use git tags as tagt for docker image.

--- devops/minikube/redeploy-backend.sh
+++ devops/minikube/redeploy-backend.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-echo "Redeploying Backend pod."
+echo "Getting latest version info from Git repository."
+git clone https://TOKEN_FROM_BITWARDEN@github.com/mild-blue/txmatching.git | true
+cd txmatching
+git fetch --all
+LATEST_TAG=`git describe --tags --abbrev=0`
+cd ..
+
+echo "Redeploying Backend."
 DEPLOYMENT_NAMESPACE=txmatch
 
-echo "Deleting pod with label 'app=backend'."
-kubectl delete pods -l app=backend --namespace="${DEPLOYMENT_NAMESPACE}"
+echo "Deleting backend deployment."
+kubectl delete -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
+
+echo "Updating tag version to ${LATEST_TAG}."
+sed "s/TAG/${LATEST_TAG}/" backend_template.yaml > backend.yaml
+
+echo "Deploying new backend version ${LATEST_TAG}."
+kubectl apply -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
 
 echo "Checking pod status and waiting until ready."
 kubectl wait --for=condition=ready pod -l app=backend --namespace="${DEPLOYMENT_NAMESPACE}" --timeout=300s
 
-echo "Backend pod is ready."
+echo "Backend is ready."
