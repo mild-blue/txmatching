79ae324dd3a373d1ac87ee9281e950fbf2492eab
Marek Polak <marazt@gmail.com>
2020-10-10 17:01:10 +0200

    Update of the deployment script to use git tags as tagt for docker image.

--- devops/minikube/redeploy-backend.sh
+++ devops/minikube/redeploy-backend.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+DEPLOYMENT_NAMESPACE="txmatch"
+GIT_TOKEN="1f69e0623d8c18e8bbd6aece7f333bdf0d212dc0"
+
+function redeploy {
+  echo "Redeploying Backend."
+
+  echo "Deleting backend deployment."
+  kubectl delete -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
+
+  echo "Updating tag version to ${LATEST_TAG}."
+  sed "s/TAG/${LATEST_TAG}/" backend_template.yaml > backend.yaml
+
+  echo "Deploying new backend version ${LATEST_TAG}."
+  kubectl apply -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
+
+  echo "Checking pod status and waiting until ready."
+  kubectl wait --for=condition=ready pod -l app=backend --namespace="${DEPLOYMENT_NAMESPACE}" --timeout=300s
+
+  echo "Backend is ready."
+}
+
 echo "Getting latest version info from Git repository."
-git clone https://TOKEN_FROM_BITWARDEN@github.com/mild-blue/txmatching.git | true
+git clone https://${GIT_TOKEN}@github.com/mild-blue/txmatching.git | true
 cd txmatching
 git fetch --all
+git reset --hard origin/master
 LATEST_TAG=`git describe --tags --abbrev=0`
 cd ..
 
-echo "Redeploying Backend."
-DEPLOYMENT_NAMESPACE=txmatch
-
-echo "Deleting backend deployment."
-kubectl delete -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
-
-echo "Updating tag version to ${LATEST_TAG}."
-sed "s/TAG/${LATEST_TAG}/" backend_template.yaml > backend.yaml
-
-echo "Deploying new backend version ${LATEST_TAG}."
-kubectl apply -f backend.yaml --namespace="${DEPLOYMENT_NAMESPACE}"
-
-echo "Checking pod status and waiting until ready."
-kubectl wait --for=condition=ready pod -l app=backend --namespace="${DEPLOYMENT_NAMESPACE}" --timeout=300s
-
-echo "Backend is ready."
+while true; do
+    read -p "Do you want to redeploy backend with version ${LATEST_TAG} [yn]?" yn
+    case $yn in
+        [Yy]* ) redeploy; break;;
+        [Nn]* ) echo "Ok, exit."; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
