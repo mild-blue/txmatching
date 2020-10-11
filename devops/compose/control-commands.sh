30a111a910406cd6b5a63a663d14a40d19a50247
Marek Polak <marazt@gmail.com>
2020-10-11 15:57:36 +0200

    Docker compose scripts added.

--- devops/compose/control-commands.sh
+++ devops/compose/control-commands.sh
@@ -13,5 +13,8 @@ docker-compose -f docker-compose.yml up -d
 # 2. Destroy
 docker-compose -f docker-compose.yml down
 
-# 2. Stop service
+# 3. Stop service
 docker-compose -f docker-compose.yml stop SERVICE_NAME
+
+# 4. Forward DB to locahost port 1234
+ssh -L 1234:localhost:5432 txmatch-USERNAME@172.17.3.14
