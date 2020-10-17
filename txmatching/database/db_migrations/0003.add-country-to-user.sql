647bd27666671e9cda951bb0e5ea1309d8a74fed
Lukas Forst <lukas@forst.pw>
2020-10-17 16:07:14 +0200

    fix postgres migration

--- txmatching/database/db_migrations/0003.add-country-to-user.sql
+++ txmatching/database/db_migrations/0003.add-country-to-user.sql
@@ -1,8 +1,8 @@
 --
 -- file: 0003.add-country-to-user.sql
--- depends: 0001.initial-schema.sql
+-- depends: 0001.initial-schema
 --
 
-ALTER TABLE public.app_user
-    -- with '_' as prefix in order to indicate private field in the python
+-- with '_' as prefix in order to indicate private field in the python
+ALTER TABLE app_user
     ADD COLUMN _allowed_edit_countries TEXT NOT NULL DEFAULT '';
