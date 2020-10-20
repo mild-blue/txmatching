9348e4462a60e28df779414f9602900b3ee7ccc0
Tomas Kourim <kourim@outlook.com>
2020-10-20 21:28:08 +0200

    review changes

--- txmatching/configuration/gives_superset_of_solutions.py
+++
@@ -1,9 +0,0 @@
-from txmatching.configuration.configuration import Configuration
-
-
-def _check_if_config_is_supported(configuration: Configuration):
-    if not (configuration.solver_constructor_name == 'AllSolutionsSolver'
-            and configuration.scorer_constructor_name == 'HLAAdditiveScorer'):
-        raise ValueError(f'Unsupported combination '
-                         f'({configuration.scorer_constructor_name}, {configuration.solver_constructor_name}) '
-                         f'of (scorer, solver)')
