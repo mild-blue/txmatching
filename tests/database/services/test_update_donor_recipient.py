37ff8f470054c2b8d38b2e2b4dd5d5574de069b0
Jan Kubant <jan.kubant@blindspot.ai>
2020-10-22 11:04:14 +0200

    Improve and refactor tests

--- tests/database/services/test_update_donor_recipient.py
+++ tests/database/services/test_update_donor_recipient.py
@@ -23,7 +23,7 @@ from txmatching.utils.get_absolute_path import get_absolute_path
 
 class TestSolveFromDbAndItsSupportFunctionality(DbTests):
     def test_saving_patients_from_obfuscated_excel(self):
-        patients = parse_excel_data(get_absolute_path('tests/test_utilities/patient_data_2020_07_obfuscated.xlsx'))
+        patients = parse_excel_data(get_absolute_path('tests/resources/patient_data_2020_07_obfuscated.xlsx'))
         txm_event = create_or_overwrite_txm_event('test')
         save_patients_from_excel_to_txm_event(patients, txm_event.db_id)
 
--- txmatching/database/services/scorer_service.py
+++ txmatching/database/services/scorer_service.py
@@ -2,10 +2,7 @@ import dataclasses
 from dataclasses import dataclass
 from typing import Dict, List
 
-from txmatching.configuration.configuration import Configuration
-from txmatching.database.services.patient_service import get_txm_event
 from txmatching.scorers.additive_scorer import ScoreMatrix
-from txmatching.scorers.scorer_from_config import scorer_from_configuration
 
 
 @dataclass
@@ -15,10 +12,3 @@ class ScoreMatrixDto:
 
 def score_matrix_to_dto(score_matrix: ScoreMatrix) -> Dict[str, Dict[str, ScoreMatrix]]:
     return dataclasses.asdict(ScoreMatrixDto(score_matrix))
-
-
-def calculate_current_score_matrix(configuration: Configuration, txm_event_db_id: int) -> ScoreMatrix:
-    scorer = scorer_from_configuration(configuration)
-    patients = get_txm_event(txm_event_db_id)
-    score_matrix = scorer.get_score_matrix(patients.donors_dict, patients.recipients_dict)
-    return score_matrix
