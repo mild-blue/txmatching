64b5dedde16a1c8829b9dcb8da2d8c053efec6ca
Jan Kubant <jan.kubant@blindspot.ai>
2020-10-24 16:59:51 +0200

    improve code in populate db and prepare it for users with different txm events

--- tests/test_utilities/populate_db.py
+++ tests/test_utilities/populate_db.py
@@ -20,22 +20,42 @@ from txmatching.web import create_app
 logger = logging.getLogger(__name__)
 ADMIN_USER = {
     'email': 'admin@example.com',
-    'password': 'admin'
+    'password': 'admin',
+    'role': UserRole.ADMIN,
+    'require_2fa': False
 }
 VIEWER_USER = {
     'email': 'viewer@example.com',
-    'password': 'viewer'
+    'password': 'viewer',
+    'role': UserRole.VIEWER,
+    'require_2fa': False
 }
 
 OTP_USER = {
     'email': 'otp@example.com',
-    'password': 'admin'
+    'password': 'admin',
+    'role': UserRole.ADMIN,
+    'phone_number': '123456789',
+    'require_2fa': False
 }
 
 SERVICE_USER = {
     'email': 'service@example.com',
-    'password': 'admin'
+    'password': 'admin',
+    'role': UserRole.SERVICE,
+    'require_2fa': False,
+    'default_txm_event_id': 1
 }
+ADMIN_WITH_DEFAULT_TXM_EVENT = {
+    'email': 'admin_default_txm_event@example.com',
+    'password': 'admin',
+    'role': UserRole.ADMIN,
+    'require_2fa': False,
+    'default_txm_event_id': 1
+}
+USERS = [
+    ADMIN_USER, SERVICE_USER, OTP_USER, ADMIN_WITH_DEFAULT_TXM_EVENT, VIEWER_USER
+]
 
 
 def create_or_overwrite_txm_event(name: str) -> TxmEvent:
@@ -52,41 +72,22 @@ def create_or_overwrite_txm_event(name: str) -> TxmEvent:
 def add_users():
     ConfigModel.query.delete()
     AppUserModel.query.delete()
-    admin = AppUserModel(
-        email=ADMIN_USER['email'],
-        pass_hash=encode_password(ADMIN_USER['password']),
-        role=UserRole.ADMIN,
-        second_factor_material=generate_totp_seed(),
-        require_2fa=False
-    )
-    viewer = AppUserModel(
-        email=VIEWER_USER['email'],
-        pass_hash=encode_password(VIEWER_USER['password']),
-        role=UserRole.VIEWER,
-        second_factor_material=generate_totp_seed(),
-        require_2fa=False
-    )
-    otp = AppUserModel(
-        email=OTP_USER['email'],
-        pass_hash=encode_password(OTP_USER['password']),
-        role=UserRole.ADMIN,
-        second_factor_material=generate_totp_seed(),
-        phone_number='123456789',
-        require_2fa=True
-    )
-    service = AppUserModel(
-        email=SERVICE_USER['email'],
-        pass_hash=encode_password(SERVICE_USER['password']),
-        role=UserRole.SERVICE,
-        second_factor_material=generate_totp_seed(),
-        require_2fa=False,
-        default_txm_event_id=1
-    )
-    _add_users([admin, viewer, otp, service])
-    ADMIN_USER['id'] = admin.id
-    VIEWER_USER['id'] = viewer.id
-    OTP_USER['id'] = otp.id
-    SERVICE_USER['id'] = service.id
+    user_models = []
+    for user in USERS:
+        user_model = AppUserModel(
+            email=user.get('email'),
+            pass_hash=encode_password(user.get('password')),
+            role=user.get('role'),
+            second_factor_material=generate_totp_seed(),
+            phone_number=user.get('phone_number'),
+            require_2fa=user.get('require_2fa'),
+            default_txm_event_id=user.get('default_txm_event_id')
+        )
+        user_models.append(user_model)
+
+    _add_users(user_models)
+    for user, user_model in zip(USERS, user_models):
+        user['id'] = user_model.id
 
 
 def _add_users(users: List[AppUserModel]):
