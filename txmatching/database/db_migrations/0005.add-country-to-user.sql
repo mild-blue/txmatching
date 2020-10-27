b4fb1dd18a4d3cd0c29ee95811588b1e6444f6a8
Lukas Forst <lukas@forst.pw>
2020-10-27 20:29:58 +0100

    delete obsolete comment

--- txmatching/database/db_migrations/0005.add-country-to-user.sql
+++ txmatching/database/db_migrations/0005.add-country-to-user.sql
@@ -3,6 +3,5 @@
 -- depends: 0004.add-unique-configuration-for-txm-event
 --
 
--- with '_' as prefix in order to indicate private field in Python
 ALTER TABLE app_user
     ADD COLUMN allowed_edit_countries jsonb not null default '[]'::jsonb
