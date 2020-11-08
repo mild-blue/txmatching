4df884a99469bdcd4ec4f00412cd62fe4b69d44b
anastasia <anastasia.surikova@akka.eu>
2020-11-08 11:25:07 +0100

    after merge fix

--- txmatching/web/frontend/src/app/components/matching-detail/matching-detail.component.ts
+++ txmatching/web/frontend/src/app/components/matching-detail/matching-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { DonorType, PatientList } from '@app/model/Patient';
 import { ListItemDetailAbstractComponent } from '@app/components/list-item/list-item.interface';
 import { Matching } from '@app/model/Matching';
