4921a80b49888c28ff9905453f778fcced5ec3f6
Lukas Forst <lukas@forst.pw>
2020-11-08 20:57:48 +0100

    fix wrong ending in migration

--- txmatching/database/db_migrations/0006.remove-active-column-from-recipient.sql
+++ txmatching/database/db_migrations/0006.remove-active-column-from-recipient.sql
@@ -1,6 +1,6 @@
 --
 -- file: txmatching/database/db_migrations/0006.remove-active-column-from-recipient.sql
--- depends: 0005.add-country-to-user.sql
+-- depends: 0005.add-country-to-user
 --
 
 ALTER TABLE recipient
