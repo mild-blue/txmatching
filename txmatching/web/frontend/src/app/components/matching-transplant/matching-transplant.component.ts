cb393268992eaa8665c081d2548dce0dc15d9bee
anastasia <anastasia.surikova@akka.eu>
2020-12-27 16:46:09 +0100

    patients tables works with new BE

--- txmatching/web/frontend/src/app/components/matching-transplant/matching-transplant.component.ts
+++ txmatching/web/frontend/src/app/components/matching-transplant/matching-transplant.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PatientService } from '@app/services/patient/patient.service';
-import { antibodiesMultipliers, compatibleBloodGroups } from '@app/model/Patient';
+import { compatibleBloodGroups } from '@app/model/Patient';
 import { Configuration } from '@app/model/Configuration';
 import { PatientList } from '@app/model/PatientList';
-import { Hla, HlaMatchType } from '@app/model/Hla';
+import { DetailedCompatibilityIndex, HlaMatchType } from '@app/model/Hla';
 import { Transplant } from '@app/model/Transplant';
 
 @Component({
@@ -17,19 +17,13 @@ export class MatchingTransplantComponent implements OnInit {
   @Input() transplant?: Transplant;
   @Input() patients?: PatientList;
 
-  public hlaScores?: Map<string, number>;
-  public totalScore: number = 0;
+  @Input() score?: number;
+  @Input() detailedCompatibilityIndex?: DetailedCompatibilityIndex[];
 
   constructor(private _patientService: PatientService) {
   }
 
   ngOnInit() {
-    this._initHLAScores();
-    this._initTotalScore();
-  }
-
-  get prefixes(): string[] {
-    return Object.keys(antibodiesMultipliers);
   }
 
   get pairBloodCompatible(): boolean {
@@ -40,43 +34,7 @@ export class MatchingTransplantComponent implements OnInit {
     return compatibleBloodGroups[this.transplant.r.parameters.blood_group].includes(this.transplant.d.parameters.blood_group);
   }
 
-  public filterCodes(codes: Hla[], prefix: string): Hla[] {
-    return codes.filter(code => code.code?.startsWith(prefix));
-  }
-
-  public filterPureCodes(codes: string[], prefix: string): string[] {
-    return codes.filter(code => code.startsWith(prefix));
-  }
-
-  public otherHLA(codes: Hla[]): Hla[] {
-    // return codes that do not start with any of prefixes
-    return codes.filter(i => {
-      for (const prefix of this.prefixes) {
-        if (!i.code || i.code.startsWith(prefix)) {
-          return false;
-        }
-      }
-      return true;
-    });
-  }
-
-  public otherHLAPure(codes: string[]): string[] {
-    // return codes that do not start with any of prefixes
-    return codes.filter(i => {
-      for (const prefix of this.prefixes) {
-        if (i.startsWith(prefix)) {
-          return false;
-        }
-      }
-      return true;
-    });
-  }
-
   public getHlaClass(match: HlaMatchType): string {
-    if (!this.transplant?.r || !match) {
-      return '';
-    }
-
     if (match === HlaMatchType.ANTIBODY) {
       // donor antigen matches some recipient antibody
       return 'bad-matching';
@@ -98,61 +56,5 @@ export class MatchingTransplantComponent implements OnInit {
 
     const currentGroup = recipientAntibodiesGroups.find(g => g.hla_group === group);
     return currentGroup?.hla_codes ?? [];
-  }
-
-
-  private _initHLAScores(): void {
-
-    if (!this.transplant?.d || !this.transplant?.r) {
-      return;
-    }
-
-    const donorAntigensCodes = this.transplant.d.parameters.hla_typing.hla_types_list.map(a => a.code);
-    const recipientAntigensCodes = this.transplant.r.parameters.hla_typing.hla_types_list.map(a => a.code);
-    // const recipientAntibodiesCodes = this.transplant.r.hla_antibodies.hla_codes_over_cutoff.map(a => a);
-    const matchingAntigens = donorAntigensCodes.filter(a => recipientAntigensCodes.includes(a));
-    // const matchingAntibodies = recipientAntibodiesCodes.filter(a => donorAntigensCodes.includes(a));
-
-    const map: Map<string, number> = new Map<string, number>();
-    for (const prefix of this.prefixes) {
-      // const prefixBadMatch = matchingAntibodies.length ? matchingAntibodies.filter(a => a?.startsWith(prefix)).length : false;
-
-      // if (prefixBadMatch) {
-      //   map.set(prefix, -1);
-      //   continue;
-      // }
-
-      const score = matchingAntigens ? matchingAntigens.filter(a => a?.startsWith(prefix)).length * antibodiesMultipliers[prefix] : 0;
-      map.set(prefix, score);
-    }
-
-    this.hlaScores = map;
-    this.totalScore = [...this.hlaScores.values()].reduce((a, b) => a + b, 0);
-  }
-
-  private _initTotalScore(): void {
-    // if transplant score exists use it as total score
-    if (this.transplant?.score !== undefined) {
-      this.totalScore = this.transplant.score;
-    }
-
-    // return if there is no donor or recipient object linked
-    if (!this.transplant?.d || !this.transplant.r) {
-      return;
-    }
-
-    // check if there is a match of recipient antibodies and donor antigens
-    const donorAntigensCodes = this.transplant.d.parameters.hla_typing.hla_types_list.map(a => a.code);
-    // for (const antibody of this.transplant.r.hla_antibodies.hla_codes_over_cutoff) {
-    //   if (donorAntigensCodes.includes(antibody)) {
-    //     this.totalScore = -1;
-    //     return;
-    //   }
-    // }
-
-    // sum all scores for matching antigens
-    if (this.hlaScores) {
-      this.totalScore = [...this.hlaScores.values()].reduce((a, b) => a + b, 0);
-    }
   }
 }
--- txmatching/web/frontend/src/app/model/Donor.ts
+++ txmatching/web/frontend/src/app/model/Donor.ts
@@ -4,6 +4,7 @@ import { DetailedCompatibilityIndex } from '@app/model/Hla';
 export interface Donor extends Patient {
   donor_type: DonorType;
   related_recipient_db_id: number;
+  score_with_related_recipient: number;
   detailed_compatibility_index_with_related_recipient: DetailedCompatibilityIndex[];
 }
 
