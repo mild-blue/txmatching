36003c458f8c1a55374e0014c5fe10c39ec603e6
Tomas Pavlin <tomas.pavlin@gmail.com>
2021-01-13 19:25:28 +0100

    Optimize filtering and sorting of matchings to improve memory consumption

--- txmatching/solve_service/solve_from_configuration.py
+++ txmatching/solve_service/solve_from_configuration.py
@@ -1,3 +1,4 @@
+import heapq
 import logging
 from typing import Iterator, List, Tuple
 
@@ -26,10 +27,8 @@ def solve_from_configuration(configuration: Configuration, txm_event_db_id: int)
     all_matchings = solver.solve()
     matching_filter = filter_from_config(configuration)
 
-    matchings_filtered, all_results_found, matching_count = _filter_and_limit_number_of_matchings(all_matchings,
-                                                                                                  matching_filter)
-
-    matchings_filtered_sorted = _sort_matchings_by_transplant_number_and_score(matchings_filtered)
+    matchings_filtered_sorted, all_results_found, matching_count = _filter_and_sort_matchings(all_matchings,
+                                                                                              matching_filter)
 
     logger.info(f'{matching_count} matchings were found.')
 
@@ -41,32 +40,34 @@ def solve_from_configuration(configuration: Configuration, txm_event_db_id: int)
                          found_matchings_count=matching_count)
 
 
-def _filter_and_limit_number_of_matchings(all_matchings: Iterator[MatchingWithScore],
-                                          matching_filter: FilterBase
-                                          ) -> Tuple[List[MatchingWithScore], bool, int]:
-    matchings = []
+def _filter_and_sort_matchings(all_matchings: Iterator[MatchingWithScore],
+                               matching_filter: FilterBase
+                               ) -> Tuple[List[MatchingWithScore], bool, int]:
+    matchings_heap = []
     all_results_found = True
     i = -1
-    # TODO here we should use something more memory efficient, never really store the whole list
-    # https://github.com/mild-blue/txmatching/issues/356
     for i, matching in enumerate(all_matchings):
         if matching_filter.keep(matching):
-            matchings.append(matching)
+            matching_entry = (
+                len(matching.get_donor_recipient_pairs()),
+                matching.score(),
+                len(matching.get_rounds()),
+                i,  # we want to skip sorting by matching
+                matching
+            )
+
+            heapq.heappush(matchings_heap, matching_entry)
+            if len(matchings_heap) > MAX_NUMBER_OF_MATCHINGS_TO_STORE:
+                heapq.heappop(matchings_heap)
+
             if i == MAX_ALLOWED_NUMBER_OF_MATCHINGS - 1:
                 logger.error(f'Max number of matchings {MAX_ALLOWED_NUMBER_OF_MATCHINGS} was reached. Returning only '
                              f'matchings found up to now.')
                 all_results_found = False
                 break
 
-    return matchings, all_results_found, i + 1
-
-
-def _sort_matchings_by_transplant_number_and_score(matchings: Iterator[MatchingWithScore]) -> List[MatchingWithScore]:
-    # TODO here we should use something more memory efficient, never really store the whole list
-    # https://github.com/mild-blue/txmatching/issues/356
-    matchings = sorted(matchings, key=lambda matching: len(matching.get_rounds()), reverse=True)
-    matchings = sorted(matchings, key=lambda matching: matching.score(), reverse=True)
-    matchings = sorted(matchings, key=lambda matching: len(matching.get_donor_recipient_pairs()), reverse=True)
+    matchings = [matching for _, _, _, _, matching in sorted(matchings_heap, reverse=True)]
     for idx, matching_in_good_order in enumerate(matchings):
         matching_in_good_order.set_order_id(idx + 1)
-    return matchings[:MAX_NUMBER_OF_MATCHINGS_TO_STORE]
+
+    return matchings, all_results_found, i + 1
