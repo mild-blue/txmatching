1c2cf9d85f9b8116c6aff981b7953887a2944611
Lukas Forst <lukas@forst.pw>
2021-01-14 11:12:02 +0100

    make pylint happy

--- txmatching/solve_service/solver_lock.py
+++ txmatching/solve_service/solver_lock.py
@@ -3,26 +3,30 @@ from typing import Callable, TypeVar
 
 from txmatching.auth.exceptions import SolverAlreadyRunningException
 
+# because these are not constants
+# pylint: disable=invalid-name
+
 _solver_running_lock = threading.Lock()
 _solver_running = False
 
 T = TypeVar('T')
 
 
-def run_with_solver_lock(fn: Callable[[], T]) -> T:
+def run_with_solver_lock(block: Callable[[], T]) -> T:
     """
     Runs execution with the lock, if some other execution is running right now,
     it raises SolverAlreadyRunningException.
     """
+    # pylint: disable=global-statement
     global _solver_running_lock, _solver_running
     try:
+        # check whether another solver is running
         with _solver_running_lock:
             if _solver_running:
                 raise SolverAlreadyRunningException()
-            else:
-                _solver_running = True
-
-        return fn()
+            _solver_running = True
+        # execute block outside of the lock
+        return block()
     finally:
         with _solver_running_lock:
             _solver_running = False
