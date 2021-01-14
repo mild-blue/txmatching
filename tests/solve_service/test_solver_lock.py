50b35fa346f45953b4952a51a490b4ffeda0a6af
Jan Kubant <kubant.jan@gmail.com>
2021-01-14 13:16:25 +0100

    Improve solver lock tests to use the same function.

--- tests/solve_service/test_solver_lock.py
+++ tests/solve_service/test_solver_lock.py
@@ -7,29 +7,32 @@ from txmatching.solve_service.solver_lock import run_with_solver_lock
 
 
 class TestSolverLock(TestCase):
+
     def test_solver_lock_should_work(self):
         model = mock.MagicMock()
-        model.run = False
+        model.run_times = 0
 
-        def test(model):
-            model.run = True
-            return 'hello'
-
-        self.assertEqual('hello', run_with_solver_lock(lambda: test(model)))
-        self.assertEqual(True, model.run)
+        self.assertEqual('hello', run_with_solver_lock(lambda: self._some_function_computing_stuff(model, 0)))
+        self.assertEqual('hello', run_with_solver_lock(lambda: self._some_function_computing_stuff(model, 0)))
+        self.assertEqual(2, model.run_times)
 
     def test_solver_lock_should_throw_exception(self):
         model = mock.MagicMock()
         model.run_times = 0
 
-        def test(model, sleep_time):
-            time.sleep(sleep_time)
-            model.run_times += 1
-
-        thread = threading.Thread(target=lambda: run_with_solver_lock(lambda: test(model, 1)), args=[])
+        thread = threading.Thread(
+            target=lambda: run_with_solver_lock(lambda: self._some_function_computing_stuff(model, 1)),
+            args=[])
         thread.start()
 
-        self.assertRaises(SolverAlreadyRunningException, lambda: run_with_solver_lock(lambda: test(model, 0)))
+        self.assertRaises(SolverAlreadyRunningException,
+                          lambda: run_with_solver_lock(lambda: self._some_function_computing_stuff(model, 0)))
 
         thread.join()
         self.assertEqual(1, model.run_times)
+
+    @staticmethod
+    def _some_function_computing_stuff(model, sleep_time: int):
+        time.sleep(sleep_time)
+        model.run_times += 1
+        return 'hello'
