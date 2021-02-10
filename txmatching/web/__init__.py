1a8e031cf1dc02bc44c23bb4cfe8b5da75d3c97f
Tomas Pavlin <tomas.pavlin@gmail.com>
2021-02-10 19:33:56 +0100

    Report http request performance

--- txmatching/web/__init__.py
+++ txmatching/web/__init__.py
@@ -1,7 +1,13 @@
+import logging
+import re
+import time
 from importlib import util as importing
+from typing import List, Tuple
 
 from flask import Flask, request, send_from_directory
 from flask_restx import Api
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from txmatching.auth.crypto import bcrypt
@@ -27,6 +33,8 @@ from txmatching.web.web_utils.logging_config import setup_logging
 
 LOGIN_MANAGER = None
 API_VERSION = '/v1'
+
+logger = logging.getLogger(__name__)
 
 
 def create_app() -> Flask:
@@ -121,6 +129,63 @@ def create_app() -> Flask:
                 response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT')
             return response
 
+    def log_request_performance():
+        class RequestPerformance:
+            def __init__(self, log_queries):
+                self._log_queries = log_queries
+                self.start()
+
+                @event.listens_for(Engine, 'before_cursor_execute')
+                def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+                    self.sql_start_time = time.perf_counter()
+
+                @event.listens_for(Engine, 'after_cursor_execute')
+                def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+                    now = time.perf_counter()
+                    last_execution_duration = now - self.sql_start_time
+                    self.sql_total_time += last_execution_duration
+
+                    self._add_sql_query(statement, last_execution_duration)
+
+            def _add_sql_query(self, query, duration) -> None:
+                query_simplified = re.sub(r'SELECT.*\sFROM', 'SELECT * FROM', str(query))
+                self.sql_queries.append((query_simplified, duration))
+
+                if self._log_queries:
+                    logger.debug(f'SQL query ({duration:.6f}s): {query_simplified}')
+
+            def start(self) -> None:
+                self.sql_queries: List[Tuple[str, float]] = []
+                self.sql_start_time = 0.0
+                self.sql_total_time = 0.0
+                self.request_start_time = time.perf_counter()
+                self.request_total_time = 0.0
+
+            def finish(self) -> None:
+                now = time.perf_counter()
+                request_duration = now - self.request_start_time
+                self.request_total_time += request_duration
+
+            def log(self):
+                logger.info(
+                    f'Request performance: sql_queries: {len(self.sql_queries)}, '
+                    f'sql_total_time: {self.sql_total_time:.6f}, '
+                    f'request_total_time: {self.request_total_time:.6f}'
+                )
+
+        # Set log_queries=True to log sql queries with duration
+        request_performance = RequestPerformance(log_queries=False)
+
+        @app.before_request
+        def before_request_callback():
+            request_performance.start()
+
+        @app.after_request
+        def after_request_callback(response):
+            request_performance.finish()
+            request_performance.log()
+            return response
+
     with app.app_context():
         load_local_development_config()
         application_config = get_application_configuration()
@@ -131,6 +196,8 @@ def create_app() -> Flask:
         register_static_proxy()
         # enable cors
         enable_cors()
+        # performance_stats
+        log_request_performance()
         # finish configuration
         configure_apis(application_config)
         return app
