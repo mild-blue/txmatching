da604bd525e2adb0edc86e0086713cac4183b8d0
Jan Kubant <kubant.jan@gmail.com>
2021-03-01 11:03:37 +0100

    Change configuration fields to required as they are always filled in for FE.

--- txmatching/data_transfer_objects/configuration/configuration_swagger.py
+++ txmatching/data_transfer_objects/configuration/configuration_swagger.py
@@ -19,59 +19,59 @@ _default_configuration = Configuration()
 ConfigurationJson = matching_api.model(
     'Configuration',
     {
-        'scorer_constructor_name': fields.String(required=False,
+        'scorer_constructor_name': fields.String(required=True,
                                                  example=_default_configuration.scorer_constructor_name),
-        'solver_constructor_name': fields.String(required=False,
+        'solver_constructor_name': fields.String(required=True,
                                                  example=_default_configuration.solver_constructor_name,
                                                  enum=[solver.__name__ for solver in SUPPORTED_SOLVERS]),
-        'require_compatible_blood_group': fields.Boolean(required=False,
+        'require_compatible_blood_group': fields.Boolean(required=True,
                                                          example=_default_configuration.require_compatible_blood_group),
-        'minimum_total_score': fields.Float(required=False, example=_default_configuration.minimum_total_score),
-        'maximum_total_score': fields.Float(required=False, example=_default_configuration.maximum_total_score),
+        'minimum_total_score': fields.Float(required=True, example=_default_configuration.minimum_total_score),
+        'maximum_total_score': fields.Float(required=True, example=_default_configuration.maximum_total_score),
         'require_better_match_in_compatibility_index': fields.Boolean(
-            required=False,
+            required=True,
             example=_default_configuration.require_better_match_in_compatibility_index
         ),
         'require_better_match_in_compatibility_index_or_blood_group': fields.Boolean(
-            required=False,
+            required=True,
             example=_default_configuration.require_better_match_in_compatibility_index_or_blood_group
         ),
         'blood_group_compatibility_bonus': fields.Float(
-            required=False,
+            required=True,
             example=_default_configuration.blood_group_compatibility_bonus
         ),
-        'use_binary_scoring': fields.Boolean(required=False, example=_default_configuration.use_binary_scoring),
-        'max_cycle_length': fields.Integer(required=False, example=_default_configuration.max_cycle_length),
-        'max_sequence_length': fields.Integer(required=False, example=_default_configuration.max_sequence_length),
+        'use_binary_scoring': fields.Boolean(required=True, example=_default_configuration.use_binary_scoring),
+        'max_cycle_length': fields.Integer(required=True, example=_default_configuration.max_cycle_length),
+        'max_sequence_length': fields.Integer(required=True, example=_default_configuration.max_sequence_length),
         'max_number_of_distinct_countries_in_round': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_number_of_distinct_countries_in_round
         ),
-        'required_patient_db_ids': fields.List(required=False, cls_or_instance=fields.Integer,
+        'required_patient_db_ids': fields.List(required=True, cls_or_instance=fields.Integer,
                                                example=_default_configuration.required_patient_db_ids),
-        'use_split_resolution': fields.Boolean(required=False, example=_default_configuration.use_split_resolution),
-        'forbidden_country_combinations': fields.List(required=False, cls_or_instance=fields.Nested(
+        'use_split_resolution': fields.Boolean(required=True, example=_default_configuration.use_split_resolution),
+        'forbidden_country_combinations': fields.List(required=True, cls_or_instance=fields.Nested(
             ForbiddenCountryCombination)),
-        'manual_donor_recipient_scores': fields.List(required=False, cls_or_instance=fields.Nested(
+        'manual_donor_recipient_scores': fields.List(required=True, cls_or_instance=fields.Nested(
             ManualDonorRecipientScoreJson)),
         'max_matchings_to_show_to_viewer': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_matchings_to_show_to_viewer
         ),
         'max_number_of_matchings': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_number_of_matchings
         ),
         'max_matchings_in_all_solutions_solver': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_matchings_in_all_solutions_solver
         ),
         'max_cycles_in_all_solutions_solver': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_cycles_in_all_solutions_solver
         ),
         'max_matchings_in_ilp_solver': fields.Integer(
-            required=False,
+            required=True,
             example=_default_configuration.max_matchings_in_ilp_solver
         )
     }
--- txmatching/web/frontend/src/app/generated/model/configurationGenerated.ts
+++ txmatching/web/frontend/src/app/generated/model/configurationGenerated.ts
@@ -14,27 +14,27 @@ import { ManualRecipientDonorScoreGenerated } from './manualRecipientDonorScoreG
 
 
 export interface ConfigurationGenerated { 
-    blood_group_compatibility_bonus?: number;
-    forbidden_country_combinations?: Array<ForbiddenCountryCombinationGenerated>;
-    manual_donor_recipient_scores?: Array<ManualRecipientDonorScoreGenerated>;
-    max_cycle_length?: number;
-    max_cycles_in_all_solutions_solver?: number;
-    max_matchings_in_all_solutions_solver?: number;
-    max_matchings_in_ilp_solver?: number;
-    max_matchings_to_show_to_viewer?: number;
-    max_number_of_distinct_countries_in_round?: number;
-    max_number_of_matchings?: number;
-    max_sequence_length?: number;
-    maximum_total_score?: number;
-    minimum_total_score?: number;
-    require_better_match_in_compatibility_index?: boolean;
-    require_better_match_in_compatibility_index_or_blood_group?: boolean;
-    require_compatible_blood_group?: boolean;
-    required_patient_db_ids?: Array<number>;
-    scorer_constructor_name?: string;
-    solver_constructor_name?: ConfigurationGeneratedSolverConstructorNameEnum;
-    use_binary_scoring?: boolean;
-    use_split_resolution?: boolean;
+    blood_group_compatibility_bonus: number;
+    forbidden_country_combinations: Array<ForbiddenCountryCombinationGenerated>;
+    manual_donor_recipient_scores: Array<ManualRecipientDonorScoreGenerated>;
+    max_cycle_length: number;
+    max_cycles_in_all_solutions_solver: number;
+    max_matchings_in_all_solutions_solver: number;
+    max_matchings_in_ilp_solver: number;
+    max_matchings_to_show_to_viewer: number;
+    max_number_of_distinct_countries_in_round: number;
+    max_number_of_matchings: number;
+    max_sequence_length: number;
+    maximum_total_score: number;
+    minimum_total_score: number;
+    require_better_match_in_compatibility_index: boolean;
+    require_better_match_in_compatibility_index_or_blood_group: boolean;
+    require_compatible_blood_group: boolean;
+    required_patient_db_ids: Array<number>;
+    scorer_constructor_name: string;
+    solver_constructor_name: ConfigurationGeneratedSolverConstructorNameEnum;
+    use_binary_scoring: boolean;
+    use_split_resolution: boolean;
 }
 export enum ConfigurationGeneratedSolverConstructorNameEnum {
     AllSolutionsSolver = 'AllSolutionsSolver',
