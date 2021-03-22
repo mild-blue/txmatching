388a2b5393a6f3ea4032c3579816ebd817bd36f7
anastasia <anastasia.surikova@akka.eu>
2021-03-22 18:28:24 +0100

    remove DEFAULT_LIST_ITEM

--- txmatching/web/frontend/src/app/components/list-item/list-item.interface.ts
+++ txmatching/web/frontend/src/app/components/list-item/list-item.interface.ts
@@ -28,12 +28,3 @@ export class ListItemDetailAbstractComponent {
               _uiInteractionsService?: UiInteractionsService) {
   }
 }
-
-// HACK: instead of using this, create proper ListItem in all places
-// where this cons is used
-export const DEFAULT_LIST_ITEM: ListItem =  {
-  index: 0,
-  isActive: undefined,
-  itemComponent: ListItemAbstractComponent,
-  detailComponent: ListItemDetailAbstractComponent
-}
