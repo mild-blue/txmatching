ebe62100d9eb3e2f77887861bc07afbeb55ff377
Tomas Pavlin <tomas.pavlin@gmail.com>
2021-03-24 13:55:49 +0100

    Rearrange configuration and fix set values button

--- txmatching/web/frontend/src/app/components/configuration/configuration.component.ts
+++ txmatching/web/frontend/src/app/components/configuration/configuration.component.ts
@@ -62,4 +62,21 @@ export class ConfigurationComponent implements OnInit {
 
     this.configForm = new FormGroup(group);
   }
+
+  public scorerNameToHint(scorerName: string): string {
+    switch(scorerName) {
+      case ScorerGenerated.SplitHlaAdditiveScorer:
+        return 'The selected scorer SplitHlaAdditiveScorer computes transplant scores by HLA matches according to their HLA groups. One match in ' +
+          'HLA group A increases score by 9, HLA match in group B increases score by 3 and HLA match in group DRB1 increases the ' +
+          'score by 1. HLA matches in other groups does not affect the score. Therefore, maximum score for each transplant is ' +
+          '9*2 + 3*2 + 2*1 = 26.';
+      case ScorerGenerated.HighResHlaAdditiveScorer:
+        return 'The selected scorer HighResHlaAdditiveScorer computes transplant scores by HLA matches according to their match types. Only matches corresponding ' +
+          'to groups A, B and DRB1 increase the score. If the antigens match through HIGH RES representation, the score is ' +
+          'increased by 3, if the antigens do not match through HIGH RES representation but they match through SPLIT representation, ' +
+          'the score is increased by 2, if the antigens match only through BROAD representation, the score is increased by 1. The maximum ' +
+          'score for this scorer is therefore 2*3 + 2*2 + 2*1 = 12.';
+      default: return 'Please select scorer.';
+    }
+  }
 }
