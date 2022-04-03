3a7a3fda60bb70b0c272e00d4871a1065b70d236
Jan Kubant <kubant.jan@gmail.com>
2022-04-03 17:29:00 +0200

    fix: delete table with parsing errors

--- txmatching/database/db_migrations/0028.drop-medical-id-add-patient-db-id-in-parsing-error.sql
+++ txmatching/database/db_migrations/0028.drop-medical-id-add-patient-db-id-in-parsing-error.sql
@@ -2,7 +2,7 @@
 -- file: txmatching/database/db_migrations/0028.drop-medical-id-add-patient-db-id-in-parsing-error
 -- depends: 0027.rename-processing-result-detail-alter-parsing-error
 --
-
+TRUNCATE TABLE parsing_error RESTART IDENTITY;
 ALTER TABLE parsing_error DROP COLUMN medical_id;
 ALTER TABLE parsing_error ADD COLUMN donor_id BIGINT default NULL;
 ALTER TABLE parsing_error ADD COLUMN recipient_id BIGINT default NULL;
