a518d9344b8d8d4646bc1fb81f1b04e7be4b8320
Ľuboš Repka <40338867+NotLubo@users.noreply.github.com>
2022-04-04 11:52:17 +0200

    #629 warning card added

---
+++ txmatching/web/frontend/src/app/components/warning/warning.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.scss']
+})
+export class WarningComponent {
+  @Input() warningType: 'warning' | 'error' | 'info' = 'info';
+  @Input() data: string[] = [];
+
+  constructor() { }
+}
