f36e75792fab2e686fbea86508d7488e91763a4c
Kristina Galikova <kristina.galik@gmail.com>
2022-06-02 10:35:00 +0100

    style: delete print statement

--- txmatching/web/frontend/src/app/parsers/version.parsers.ts
+++ txmatching/web/frontend/src/app/parsers/version.parsers.ts
@@ -2,7 +2,6 @@ import { VersionGenerated, VersionGeneratedColourSchemeEnum, VersionGeneratedEnv
 import { ColourScheme, EnvironmentType, Version } from '../model';
 
 export const parseVersion = (data: VersionGenerated): Version => {
-  console.log('im here in parse version', parseEnvironmentType(data.environment), 'from', data.environment, parseColourScheme(data.colour_scheme), 'from', data.colour_scheme);
   return {
     colour_scheme: parseColourScheme(data.colour_scheme),
     environment: parseEnvironmentType(data.environment),
