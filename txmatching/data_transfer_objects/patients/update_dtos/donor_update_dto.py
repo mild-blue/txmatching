6fa70a1841b354a200f21fd6368bb35d7248a1e3
Kristina Galikova <kristina.galik@gmail.com>
2022-06-21 18:48:01 +0100

    refactor: concentrate validation of patient parameters in patient.py

--- txmatching/data_transfer_objects/patients/update_dtos/donor_update_dto.py
+++ txmatching/data_transfer_objects/patients/update_dtos/donor_update_dto.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
-from datetime import date
 from typing import Optional
 
-from txmatching.auth.exceptions import InvalidArgumentException
 from txmatching.data_transfer_objects.patients.update_dtos.patient_update_dto import \
     PatientUpdateDTO
+from txmatching.patients.patient import is_height_valid, is_weight_valid, is_year_of_birth_valid
 
 
 @dataclass
@@ -12,11 +11,11 @@ class DonorUpdateDTO(PatientUpdateDTO):
     active: Optional[bool] = None
 
     def __post_init__(self):
-        if self.height and self.height < 0:
-            raise InvalidArgumentException(f'Invalid donor height {self.height}cm.')
+        if self.height:
+            is_height_valid("donor", self.height)
 
-        if self.weight and self.weight < 0:
-            raise InvalidArgumentException(f'Invalid donor weight {self.weight}kg.')
+        if self.weight:
+            is_weight_valid("donor", self.weight)
 
-        if self.year_of_birth and (self.year_of_birth < 1900 or self.year_of_birth > date.today().year):
-            raise InvalidArgumentException(f'Invalid donor year of birth {self.year_of_birth}')
+        if self.year_of_birth:
+            is_year_of_birth_valid("donor", self.year_of_birth)
