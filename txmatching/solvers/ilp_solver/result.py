3b9307307399fa51fe91c972b5ff8b71992526ba
Kristina Galikova <kristina.galik@gmail.com>
2022-07-05 20:18:53 +0100

    fix: delete unused classes

--- txmatching/solvers/ilp_solver/result.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from txmatching.solvers.ilp_solver.solution import Solution, Status
-
-# this is not used anywhere, do I keep it for future possibilities or not
-@dataclass
-class Result:
-    status: Status
-    solution: Solution = None
