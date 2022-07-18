ff1a50d38366f35b54fc3494aafeef0ef82af334
krllstdn <kirill@DESKTOP-VU30TP0.localdomain>
2022-07-18 22:01:19 +0200

    made points 2 and 4

--- txmatching/data_transfer_objects/shared_dto.py
+++ txmatching/data_transfer_objects/shared_dto.py
@@ -11,3 +11,13 @@ class IdentifierDTOIn:
     # pylint:disable=invalid-name
     id: int
     # pylint:enable=invalid-name
+
+    def __post_init__(self):
+        if self.id:
+            is_id_valid(self.id)
+
+
+def is_id_valid(id: int):
+    if id < 0:
+        raise ValueError("Invalid id, it must be greater than 0")
+
