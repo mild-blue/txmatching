835927a3f814cfa8a2665320bcfb8beda29f13d2
Kristina Galikova <kristina.galik@gmail.com>
2022-07-20 15:04:05 +0100

    docs: add comment explaining default value attribute

--- txmatching/data_transfer_objects/patients/out_dtos/donor_dto_out.py
+++ txmatching/data_transfer_objects/patients/out_dtos/donor_dto_out.py
@@ -14,7 +14,9 @@ class DonorDTOOut(Donor):
     max_score_with_related_recipient: Optional[float] = None
     detailed_score_with_related_recipient: Optional[List[DetailedScoreForHLAGroup]] = None
     compatible_blood_with_related_recipient: Optional[str] = None
-    active_and_valid_pair: Optional[bool] = None
+    # this attribute has default value because fields without default values cannot appear 
+    # after fields with default values. That is why we check if this attribute is set during init
+    active_and_valid_pair: bool = None
 
     def __post_init__(self):
         if self.active_and_valid_pair is None:
