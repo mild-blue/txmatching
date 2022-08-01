5eb8ec71d6bb3ef0d9626414d02d1268f709a4c8
krllstdn <kirill@DESKTOP-VU30TP0.localdomain>
2022-08-01 12:08:29 +0200

    removed unnecessary comments, small corrections

--- txmatching/data_transfer_objects/shared_dto.py
+++ txmatching/data_transfer_objects/shared_dto.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from txmatching.database.services.txm_event_service import does_txm_event_exist
 from txmatching.configuration.configuration import does_configuration_exist
+
 
 @dataclass
 class SuccessDTOOut:
@@ -13,9 +13,5 @@ class IdentifierDTOIn:
     id: int
     # pylint:enable=invalid-name
 
-    def __post_init__(self): # to do: check whether the identifier exists (and check all other id fields)
+    def __post_init__(self):
         does_configuration_exist(self.id)
-
-
-# def does_configuration_exist(configuration_id):
-#     return ConfigModel.query.filter_by(id=configuration_id).first() is not None
