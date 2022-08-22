775dbbac8f0d7a090a05350c043ec3a02bf6a0a0
Kristina Galikova <kristina.galik@gmail.com>
2022-08-22 11:05:49 +0100

    feat: add export endpoint to optimization

--- txmatching/optimizer/optimizer_functions.py
+++ txmatching/optimizer/optimizer_functions.py
@@ -1,4 +1,10 @@
+from typing import Dict, List, Optional
+
+from txmatching.configuration.configuration import Configuration
 from txmatching.optimizer.optimizer_return_object import CycleOrChain, DonorToRecipient, OptimizerReturn, Statistics
+from txmatching.optimizer.optimizer_request_object import Limitations, OptimizerConfiguration, Pair
+from txmatching.patients.patient_types import DonorDbId
+from txmatching.patients.patient import Donor
 
 
 def export_return_data() -> OptimizerReturn:
@@ -10,4 +16,21 @@ def export_return_data() -> OptimizerReturn:
             CycleOrChain(donor_to_recipient_list, [1, 2, 5])
         ],
         statistics=Statistics(1, 2)
+    )
+
+
+def get_pairs_from_txm_event(donors: Dict[DonorDbId, Donor]) -> List[Pair]:
+    pairs = [Pair(donor_id=donor_id, recipient_id=donor.related_recipient_db_id) for donor_id, donor in donors.items()]
+    return pairs
+
+
+def get_optimizer_configuration(config: Optional[Configuration]) -> OptimizerConfiguration:
+    limitations = Limitations(
+        max_cycle_length=config.parameters.max_cycle_length if config else 4,
+        max_chain_length=config.parameters.max_sequence_length if config else 4,
+        custom_algorithm_settings={}
+    )
+    return OptimizerConfiguration(
+        limitations=limitations,
+        scoring=None
     )
--- txmatching/web/api/optimizer_api.py
+++ txmatching/web/api/optimizer_api.py
@@ -1,12 +1,21 @@
 from flask_restx import Resource
+from typing import Optional
 
+from txmatching.data_transfer_objects.configuration.configuration_swagger import \
+    ConfigIdPathParamDefinition
 from txmatching.data_transfer_objects.optimizer.optimizer_in_swagger import \
     OptimizerReturnObjectJson, OptimizerRequestObjectJson
-from txmatching.optimizer.optimizer_functions import export_return_data
+from txmatching.database.services.config_service import \
+    get_configuration_from_db_id_or_default
+from txmatching.database.services.txm_event_service import \
+    get_txm_event_complete
+from txmatching.optimizer.optimizer_functions import export_return_data, get_optimizer_configuration, \
+    get_pairs_from_txm_event
 from txmatching.optimizer.optimizer_request_object import OptimizerRequest
 from txmatching.web.web_utils.namespaces import optimizer_api
 from txmatching.web.web_utils.route_utils import request_body, response_ok
 
+EXPORT_DESCRIPTION = "Endpoint that exports data from TXM that can be input into optimization module"
 OPTIMIZER_DESCRIPTION = "Endpoint that calculates matchings from compatibility graph and configuration"
 
 
@@ -24,3 +33,41 @@ class Optimize(Resource):
         # todo return files
         optimizer_return = export_return_data()
         return response_ok(optimizer_return)
+
+
+@optimizer_api.route('/export/<int:txm_event_id>/<int:config_id>', methods=['GET'])
+class Optimize(Resource):
+    @optimizer_api.doc(
+        params={
+            'matching_id': {
+                'description': 'Id of txm event chosen',
+                'type': int,
+                'in': 'path',
+                'required': True
+            },
+            'config_id': ConfigIdPathParamDefinition
+        }
+    )
+    @optimizer_api.response_ok(OptimizerRequestObjectJson, description=EXPORT_DESCRIPTION)
+    @optimizer_api.response_errors()
+    @optimizer_api.require_user_login()
+    def get(self, txm_event_id: int, config_id: Optional[int]) -> str:
+        txm_event = get_txm_event_complete(txm_event_id)
+        txm_event_configuration = get_configuration_from_db_id_or_default(txm_event, config_id)
+
+        # get compatibility graph
+        # todo co dat sem okrem hla? -> spyraj sa madarov
+        compatibility_graph = None
+
+        # get pairs
+        pairs = get_pairs_from_txm_event(txm_event.active_and_valid_donors_dict)
+        print(pairs)
+
+        # get configuration
+        configuration = get_optimizer_configuration(txm_event_configuration)
+
+        return response_ok(OptimizerRequest(
+            compatibility_graph=compatibility_graph,
+            pairs=pairs,
+            configuration=configuration
+        ))
