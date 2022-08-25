a54b51ef95573739700f811bad601bef784bd9c6
Kristina Galikova <kristina.galik@gmail.com>
2022-08-25 13:51:05 +0100

    fix: make optimizer endpoint work

--- txmatching/optimizer/optimizer_functions.py
+++ txmatching/optimizer/optimizer_functions.py
@@ -1,11 +1,11 @@
 import networkx as nx
 import numpy as np
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
 
 from tests.test_utilities.hla_preparation_utils import create_hla_typing
 from txmatching.configuration.config_parameters import ConfigParameters
 from txmatching.filters.filter_from_config import filter_from_config
-from txmatching.optimizer.optimizer_return_object import OptimizerReturn
+from txmatching.optimizer.optimizer_return_object import CycleOrChain, DonorToRecipient, OptimizerReturn, Statistics
 from txmatching.optimizer.optimizer_request_object import OptimizerRequest, Pair
 from txmatching.patients.hla_model import HLAAntibodies
 from txmatching.patients.patient_parameters import PatientParameters
@@ -15,7 +15,10 @@ from txmatching.solve_service.solve_from_configuration import filter_and_sort_ma
 from txmatching.solve_service.solver_lock import run_with_solver_lock
 from txmatching.solvers.ilp_solver.txm_configuration_for_ilp import \
     DataAndConfigurationForILPSolver
+from txmatching.solvers.matching.matching_with_score import MatchingWithScore
 from txmatching.solvers.solver_from_config import solver_from_configuration
+from txmatching.solvers.matching.transplant_cycle import TransplantCycle
+from txmatching.solvers.matching.transplant_sequence import TransplantSequence
 from txmatching.utils.blood_groups import BloodGroup
 from txmatching.utils.country_enum import Country
 from txmatching.utils.enums import Solver
@@ -30,7 +33,9 @@ def _calculate_from_optimizer_unsafe(optimizer_request_object: OptimizerRequest)
         # todo add more parameters later according to added keywords
         solver_constructor_name=Solver.ILPSolver,
         max_cycle_length=optimizer_request_object.configuration.limitations.max_cycle_length,
-        max_sequence_length=optimizer_request_object.configuration.limitations.max_chain_length
+        max_sequence_length=optimizer_request_object.configuration.limitations.max_chain_length,
+        # todo ??
+        max_number_of_matchings=1
     )
 
     donors_dict, recipients_dict = _create_patients(optimizer_request_object.pairs)
@@ -41,13 +46,12 @@ def _calculate_from_optimizer_unsafe(optimizer_request_object: OptimizerRequest)
                                        recipients_dict=recipients_dict,
                                        scorer=scorer)
 
-    # TODO is this needed?
     solver.score_matrix = _create_score_matrix(donors_dict, recipients_dict,
                                                optimizer_request_object.compatibility_graph)
 
-    donor_to_recipient = {pair.donor_id: pair.recipient_id for pair in optimizer_request_object.pairs}
     data_and_configuration = DataAndConfigurationForILPSolver(donors_dict, recipients_dict, config_parameters)
 
+    donor_to_recipient = {pair.donor_id: pair.recipient_id for pair in optimizer_request_object.pairs}
     comp_matrix = _get_donor_score_matrix(optimizer_request_object.compatibility_graph, donor_to_recipient)
 
     data_and_configuration.graph = _create_graph(comp_matrix, data_and_configuration.non_directed_donors,
@@ -59,13 +63,13 @@ def _calculate_from_optimizer_unsafe(optimizer_request_object: OptimizerRequest)
 
     matchings_filtered_sorted, _, _ = filter_and_sort_matchings(all_matchings, matching_filter, config_parameters)
 
-    print("im here at the end with these matchings", len(matchings_filtered_sorted))
-    for matching in matchings_filtered_sorted:
-        print("this is a mathcing")
-        for pair in matching.matching_pairs:
-            print((pair.donor.db_id, pair.recipient.db_id))
-    # TODO
-    return None
+    if len(matchings_filtered_sorted) == 0:
+        return OptimizerReturn(cycles_and_chains=[], statistics=Statistics(0, 0))
+
+    donor_to_enum = {donor.db_id: i for i, donor in enumerate(donors_dict.values())}
+    recipient_to_enum = {recipient.db_id: i for i, recipient in enumerate(recipients_dict.values())}
+    return _get_optimizer_return_object(matchings_filtered_sorted[0], solver.score_matrix, donor_to_enum,
+                                        recipient_to_enum)
 
 
 def _create_patients(pairs: List[Pair]) -> Tuple[Dict[int, Donor], Dict[int, Recipient]]:
@@ -76,7 +80,8 @@ def _create_patients(pairs: List[Pair]) -> Tuple[Dict[int, Donor], Dict[int, Rec
         if pair.donor_id not in donors_dict:
             donors_dict[pair.donor_id] = _create_empty_donor(pair)
         if pair.recipient_id is not None and pair.recipient_id not in recipients_dict:
-            list_of_donors = [pair.donor_id for inner_pair in pairs if inner_pair.recipient_id == pair.recipient_id]
+            list_of_donors = [inner_pair.donor_id for inner_pair in pairs if
+                              inner_pair.recipient_id == pair.recipient_id]
             recipients_dict[pair.recipient_id] = _create_empty_recipient(pair, list_of_donors)
 
     return donors_dict, recipients_dict
@@ -107,7 +112,7 @@ def _create_empty_recipient(pair: Pair, list_of_donors: List[int]) -> Recipient:
 
 
 def _create_empty_parameters():
-    # TODO maybe change this to something sensible
+    # todo maybe change this to something more sensible
     return PatientParameters(
         blood_group=BloodGroup.A,
         country_code=Country.AZE,
@@ -172,3 +177,52 @@ def _get_donor_score_matrix(comp_graph: List[Dict[str, int]], donor_to_recipient
     score_matrix = np.array(score_matrix)
 
     return score_matrix
+
+
+def _get_optimizer_return_object(matching: MatchingWithScore, score_matrix: np.array, donor_dict: Dict[int, int],
+                                 recipient_dict: Dict[int, int]) -> OptimizerReturn:
+    statistics = _get_statistics(matching)
+
+    cycles = matching.get_cycles()
+    chains = matching.get_sequences()
+
+    # todo: toto ma reprezentovat len jeden matching composed of several cycles and chains hej? (just checking)
+    cycles_and_chains = []
+
+    for cycle_or_chain in cycles + chains:
+        patients = _get_patients_for_cycle_or_chain(cycle_or_chain, score_matrix, donor_dict, recipient_dict)
+        scores = _get_scores(patients)
+        cycle_object = CycleOrChain(
+            patients=patients,
+            scores=scores
+        )
+        cycles_and_chains.append(cycle_object)
+
+    return OptimizerReturn(
+        cycles_and_chains=cycles_and_chains,
+        statistics=statistics
+    )
+
+
+def _get_patients_for_cycle_or_chain(cycle_or_chain: Union[TransplantCycle, TransplantSequence], score_matrix: np.array,
+                                     donor_dict: Dict[int, int], recipient_dict: Dict[int, int]) -> List[
+    DonorToRecipient]:
+    return [
+        DonorToRecipient(
+            pair.donor.db_id,
+            pair.recipient.db_id,
+            [int(score_matrix[donor_dict[pair.donor.db_id]][recipient_dict[pair.recipient.db_id]])]
+        ) for pair in cycle_or_chain.donor_recipient_pairs
+    ]
+
+
+def _get_statistics(matching: MatchingWithScore) -> Statistics:
+    return Statistics(
+        number_of_found_cycles=len(matching.get_cycles()) + len(matching.get_sequences()),
+        number_of_found_transplants=len(matching.matching_pairs)
+    )
+
+
+def _get_scores(patient_pairs: List[DonorToRecipient]) -> List[int]:
+    scores = np.array([pair.score for pair in patient_pairs])
+    return np.sum(scores, axis=0).tolist()
