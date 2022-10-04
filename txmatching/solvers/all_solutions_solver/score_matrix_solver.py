45db924749fcb12136aa9cd6e4bb7533658066ed
krllstdn <kirill@DESKTOP-VU30TP0.localdomain>
2022-10-04 17:22:43 +0200

    First test 'handling_correctly_multiple_donors_wiht_the_same_recipient' doesn't fail

--- txmatching/solvers/all_solutions_solver/score_matrix_solver.py
+++ txmatching/solvers/all_solutions_solver/score_matrix_solver.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, Iterable, List
+from typing import Dict, Iterable, List, Tuple
 
 import numpy as np
 from graph_tool import topology
@@ -38,7 +38,7 @@ def find_possible_path_combinations_from_score_matrix(score_matrix: np.ndarray,
         return
 
     donor_idx_to_recipient_idx = get_donor_idx_to_recipient_idx(score_matrix)
-    highest_scoring_paths = get_highest_scoring_paths(score_matrix,              # get_compatible_donor_idxs_per_donor_idx() is used in this function
+    donor_idx_to_recipient_idx, highest_scoring_paths = get_highest_scoring_paths(score_matrix,
                                                       donor_idx_to_recipient_idx,
                                                       donors,
                                                       config_parameters)
@@ -62,22 +62,25 @@ def find_possible_path_combinations_from_score_matrix(score_matrix: np.ndarray,
         max_cliques = []
 
     logger.info('Creating pairings from paths and circuits ')
-
+    
+    #the problem is somewhere here - cycles are good, so it must be here
     for clique in max_cliques:
         unused_path_numbers.difference_update(clique)
-        yield get_pairs_from_clique(clique, path_number_to_path, donor_idx_to_recipient_idx)
-
+        pairs = get_pairs_from_clique(clique, path_number_to_path, donor_idx_to_recipient_idx)
+        yield pairs
+    logger.info("bla")
     single_vertex_cliques = [[path_number] for path_number in unused_path_numbers]
     for clique in single_vertex_cliques:
-        yield get_pairs_from_clique(clique,
+        pairs = get_pairs_from_clique(clique,
                                     path_number_to_path,
                                     donor_idx_to_recipient_idx)
-
+        yield pairs
+    logger.info("bla")
 
 def get_highest_scoring_paths(score_matrix: np.ndarray,
                               donor_idx_to_recipient_idx: Dict[int, int],
                               donors: List[Donor],
-                              config_parameters: ConfigParameters = ConfigParameters()) -> List[Path]:
+                              config_parameters: ConfigParameters = ConfigParameters()) -> Tuple[Dict[int, int], List[Path]]:
     n_donors, _ = score_matrix.shape
     assert len(donors) == n_donors
 
@@ -95,12 +98,13 @@ def get_highest_scoring_paths(score_matrix: np.ndarray,
                                    donors,
                                    config_parameters.max_number_of_distinct_countries_in_round)
 
-    cycles_with_recipients = [[[donor_idx, donor_idx_to_recipient_idx[donor_idx]]
-                               for donor_idx in cycle] for cycle in cycles]
+    
+    # TODO: MUZEME SE POKOUSIT ODFILTROVAT NEVALIDNI PARY I Z donor_idx_to_recipient_idx
 
-    valid_cycles = _filter_cycles_with_recipient_duplicates(cycles_with_recipients)
+    donor_idx_to_recipient_idx, valid_cycles = _filter_cycles_with_recipient_duplicates(donor_idx_to_recipient_idx, cycles)
+    
 
-    highest_scoring_paths = keep_only_highest_scoring_paths(
+    highest_scoring_paths = keep_only_highest_scoring_paths( # pada zde 
         valid_cycles,
         score_matrix=score_matrix,
         donor_idx_to_recipient_idx=donor_idx_to_recipient_idx
@@ -109,14 +113,45 @@ def get_highest_scoring_paths(score_matrix: np.ndarray,
         score_matrix=score_matrix,
         donor_idx_to_recipient_idx=donor_idx_to_recipient_idx
     )
-    return highest_scoring_paths
+    return donor_idx_to_recipient_idx, highest_scoring_paths
 
 
-def _filter_cycles_with_recipient_duplicates(cycles_with_recipients: List[List[int]]) -> List[List[int]]:
+def _filter_cycles_with_recipient_duplicates(donor_idx_to_recipient_idx: Dict[int, int], cycles: List[Path]) -> Tuple[Dict[int, int], List[Path]]:
+    cycles_with_recipients = [[[donor_idx, donor_idx_to_recipient_idx[donor_idx]]
+                               for donor_idx in cycle] for cycle in cycles]
     valid_cycles = []
     for cycle in cycles_with_recipients:
         recipient_ids = [pair[1] for pair in cycle]
+        recipient_ids.pop()
         unique_recipients = set(recipient_ids)
         if len(unique_recipients) == len(recipient_ids):
             valid_cycles.append(cycle)
-    return valid_cycles
+
+    # get list of all recipients from valid cycles
+    # all_donor_recipients = [pair for cycle in valid_cycles for pair in cycle[:-1]]
+    # seen_recipients = []
+    # for pair in all_donor_recipients:
+    #     if pair[1] in seen_recipients:
+    #         donor_idx_to_recipient_idx.pop(pair[0])
+    #     else:
+    #         seen_recipients.append(pair[1])
+
+    valid_cycles = [tuple(donor_idx for donor_idx, _ in cycle) for cycle in valid_cycles]
+
+    return donor_idx_to_recipient_idx, valid_cycles
+
+
+    # remove pairs from donor_idx_to_recipient_idx
+    '''
+    if recipient is twice in the group of cycles then remove the second pair from donor_idx_to_recipient_idx
+    '''
+    # seen_recipients = set()
+    # for cycle in valid_cycles:
+    #     recipient_ids = [pair[1] for pair in cycle]
+    #     recipient_ids.pop()
+    #     dup = [x for x in recipient_ids if recipient_ids.count(x) > 1]
+    #     if len(dup) > 0:
+    #         for pair in cycle:
+    #             if pair[1] in dup:
+    #                 donor_idx_to_recipient_idx.pop(pair[0]) #invalid pop
+    #                 break
