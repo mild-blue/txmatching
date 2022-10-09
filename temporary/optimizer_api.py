3d9005235f1dac2d39251153dc52a24f52a4f1e4
Kristina Galikova <kristina.galik@gmail.com>
2022-10-09 10:20:30 +0100

    threadsafe

--- temporary/optimizer_api.py
+++ temporary/optimizer_api.py
@@ -1,9 +1,11 @@
 from enum import Enum
 from flask import jsonify, make_response, Response, request
 from flask_restx import Resource
+from typing import Callable, TypeVar
 
 import json
 import os
+import threading
 
 from dacite import Config, Type, from_dict
 from namespace import Namespace
@@ -12,7 +14,6 @@ from optimiser import input_path, main, output_path
 from optimizer_in_swagger import OptimizerReturnObjectJson, OptimizerRequestObjectJson
 from optimizer_request_object import OptimizerRequest
 from optimizer_return_object import OptimizerReturn
-from typing import TypeVar
 
 OPTIMIZER_DESCRIPTION = "Endpoint that calculates matchings from compatibility graph and configuration"
 INPUT_PATH = input_path
@@ -20,6 +21,9 @@ OUTPUT_PATH = output_path
 OPTIMIZER_NAMESPACE = 'optimizer'
 optimizer_api = Namespace(OPTIMIZER_NAMESPACE)
 T = TypeVar('T')
+_solver_running_lock = threading.Lock()
+_solver_running = False
+
 
 def _convert_json_to_dataclass(data_class: Type[T], json_file) -> T:
     return from_dict(
@@ -27,6 +31,40 @@ def _convert_json_to_dataclass(data_class: Type[T], json_file) -> T:
         data=json_file,
         config=Config(cast=[Enum])
     )
+
+
+def run_with_solver_lock(block: Callable[[], T]) -> T:
+    global _solver_running_lock, _solver_running
+    try:
+        with _solver_running_lock:
+            if _solver_running:
+                raise Exception('Optimizer is already running.')
+            _solver_running = True
+        return block()
+    finally:
+        with _solver_running_lock:
+            _solver_running = False
+
+
+def solve(optimizer_request):
+    with open(INPUT_PATH, 'w', encoding='utf-8') as f:
+        json.dump(optimizer_request, f, ensure_ascii=False, indent=4)
+
+    main(INPUT_PATH, OUTPUT_PATH)
+
+    output_file = open(OUTPUT_PATH)
+    optimizer_return = json.load(output_file)
+
+    os.remove(INPUT_PATH)
+    os.remove(OUTPUT_PATH)
+
+    # check whether the input is in correct format
+    _convert_json_to_dataclass(OptimizerRequest, optimizer_request)
+    # check whether the output is un correct format
+    _convert_json_to_dataclass(OptimizerReturn, optimizer_return)
+
+    return optimizer_return
+
 
 # https://www.dropbox.com/home/KEP-SOFT_developers/optimizer_module?preview=Optimizer+Input+Schema+First+draft.docx&preview=Optimizer+Input+Schema+First+draft.docx
 @optimizer_api.route('', methods=['POST'])
@@ -36,20 +74,6 @@ class Optimize(Resource):
     def post(self) -> Response:
         optimizer_request = request.json
 
-        with open(INPUT_PATH, 'w', encoding='utf-8') as f:
-            json.dump(optimizer_request, f, ensure_ascii=False, indent=4)
-
-        main(INPUT_PATH, OUTPUT_PATH)
-
-        output_file = open(OUTPUT_PATH)
-        optimizer_return = json.load(output_file)
-
-        os.remove(INPUT_PATH)
-        os.remove(OUTPUT_PATH)
-
-        # check whether the input is in correct format
-        _convert_json_to_dataclass(OptimizerRequest, optimizer_request)
-        # check whether the output is un correct format
-        _convert_json_to_dataclass(OptimizerReturn, optimizer_return)
+        optimizer_return = run_with_solver_lock(lambda: solve(optimizer_request))
 
         return make_response(jsonify(optimizer_return))
