07e719ad4632a99a27756caebe6c0551b516f177
Kristina Galikova <kristina.galik@gmail.com>
2022-11-13 23:10:35 +0000

    fix: rebase issues

--- txmatching/web/api/matching_api.py
+++ txmatching/web/api/matching_api.py
@@ -3,16 +3,14 @@ from typing import Optional
 
 from flask_restx import Resource
 
-from txmatching.auth.auth_check import require_valid_txm_event_id
+from txmatching.auth.auth_check import require_valid_config_id, require_valid_txm_event_id
 from txmatching.auth.data_types import UserRole
 from txmatching.auth.request_context import get_user_role
-from txmatching.configuration.config_parameters import ConfigParameters
-from txmatching.data_transfer_objects.configuration.configuration_swagger import \
-    ConfigurationJson
+from txmatching.data_transfer_objects.configuration.configuration_swagger import (
+    ConfigIdPathParamDefinition)
 from txmatching.data_transfer_objects.matchings.matching_swagger import \
     CalculatedMatchingsJson
-from txmatching.database.services.config_service import \
-    get_config_for_parameters_or_save
+from txmatching.database.services.config_service import get_configuration_from_db_id_or_default
 from txmatching.database.services.matching_service import (
     create_calculated_matchings_dto,
     get_matchings_detailed_for_pairing_result_model)
@@ -20,28 +18,26 @@ from txmatching.database.services.pairing_result_service import \
     get_pairing_result_comparable_to_config_or_solve
 from txmatching.database.services.txm_event_service import \
     get_txm_event_complete
-from txmatching.utils.logged_user import get_current_user_id
 from txmatching.web.web_utils.namespaces import matching_api
 from txmatching.web.web_utils.route_utils import response_ok
 
 logger = logging.getLogger(__name__)
 
 
-@matching_api.route('/calculate-for-config', methods=['POST'])
+@matching_api.route('/calculate-for-config/<config_id>', methods=['GET'])
 class CalculateFromConfig(Resource):
+    @matching_api.doc(
+        params={'config_id': ConfigIdPathParamDefinition},
+    )
     @matching_api.require_user_login()
-    @matching_api.request_body(ConfigurationJson)
     @matching_api.response_ok(CalculatedMatchingsJson, 'List of all matchings for given configuration.')
     @matching_api.response_errors(exceptions=set(), add_default_namespace_errors=True)
     @require_valid_txm_event_id()
-    def post(self, txm_event_id: int) -> str:
-        configuration_parameters = request_body(ConfigParameters)
-
+    @require_valid_config_id()
+    def get(self, txm_event_id: int, config_id: Optional[int]) -> str:
         txm_event = get_txm_event_complete(txm_event_id)
-        user_id = get_current_user_id()
-
-        # 1. Get or save config
-        configuration = get_config_for_parameters_or_save(configuration_parameters, txm_event.db_id, user_id)
+        # 1. Get configuration from db
+        configuration = get_configuration_from_db_id_or_default(txm_event=txm_event, configuration_db_id=config_id)
 
         # 2. Get or solve pairing result
         pairing_result_model = get_pairing_result_comparable_to_config_or_solve(configuration, txm_event)
@@ -51,6 +47,7 @@ class CalculateFromConfig(Resource):
 
         calculated_matchings_dto = create_calculated_matchings_dto(matchings_detailed, matchings_detailed.matchings,
                                                                    configuration.id)
+        configuration_parameters = configuration.parameters
         calculated_matchings_dto.calculated_matchings = calculated_matchings_dto.calculated_matchings[
             :configuration_parameters.max_number_of_matchings]
         if get_user_role() == UserRole.VIEWER:
