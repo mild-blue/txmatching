feb12a18309532e53e82fbedccedb5aedab05164
Kristina Galikova <kristina.galik@gmail.com>
2022-11-20 20:01:16 +0000

    docs: update hlaantibodyin description

--- txmatching/data_transfer_objects/base_patient_swagger.py
+++ txmatching/data_transfer_objects/base_patient_swagger.py
@@ -76,7 +76,7 @@ HLAAntibodyJsonIn = public_api.model('HLAAntibodyIn', {
                           description='HLA antibody name in high (A\\*01:01), low (A\\*01), split (A1) or broad (A9) '
                                       'resolutions. In case of DP or DQ, when alpha and beta are provided, the expected'
                                       ' format is DQ[01:01,02:02] equivalently DP[01:01,02:02], and will result in two '
-                                      'separate antibodies with the same MFI.'),
+                                      'separate antibodies.'),
     'mfi': fields.Integer(required=True, example=2350, description='Mean fluorescence intensity. Use exact value.'),
     'cutoff': fields.Integer(required=True,
                              example=2000,
