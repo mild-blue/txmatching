7fa8c52befe20c7dcc1163ad11eaba9062314768
Kristina Galikova <kristina.galik@gmail.com>
2023-02-13 13:05:14 +0000

    docs: create an issue for max transplant score

--- txmatching/scorers/hla_additive_scorer.py
+++ txmatching/scorers/hla_additive_scorer.py
@@ -26,7 +26,7 @@ class HLAAdditiveScorer(AdditiveScorer, ABC):
 
     @property
     def max_transplant_score(self) -> float:
-        # TODO?
+        # TODO https://github.com/mild-blue/txmatching/issues/1152
         max_value = max(self.ci_configuration.match_type_bonus.values()) * sum(
             bonus * HLA_GROUPS_PROPERTIES[group].max_count_per_patient for group, bonus in
             self.ci_configuration.hla_typing_bonus_per_groups_without_dp_dq.items()) \
