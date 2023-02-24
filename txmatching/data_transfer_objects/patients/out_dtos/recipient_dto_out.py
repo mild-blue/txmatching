4adad8bacab38dfdb1bf649ac6f3a2b5d1f1e4b1
Jakub Monhart <monhart.jakub@gmail.com>
2023-02-24 14:28:24 +0100

    Get detailed compatibility info for donors compatible with recipient.

--- txmatching/data_transfer_objects/patients/out_dtos/recipient_dto_out.py
+++ txmatching/data_transfer_objects/patients/out_dtos/recipient_dto_out.py
@@ -3,11 +3,14 @@ from typing import Dict, List, Optional
 
 from txmatching.data_transfer_objects.hla.parsing_issue_dto import ParsingIssue
 from txmatching.patients.patient import Recipient
+from txmatching.utils.recipient_donor_compatibility_details import RecipientDonorCompatibilityDetails
 
 
 @dataclass
 class RecipientDTOOut(Recipient):
     all_messages: Optional[Dict[str, List[ParsingIssue]]] = None
+    cpra: Optional[float] = None
+    compatible_donors_details: Optional[List[RecipientDonorCompatibilityDetails]] = None
 
 
 @dataclass
