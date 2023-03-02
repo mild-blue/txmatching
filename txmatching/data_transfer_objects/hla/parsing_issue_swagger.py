ce864b4c0c44c8b577b601d69faf08cd7872678a
Kyrylo Stadniuk <krllstdn@gmail.com>
2023-03-02 08:16:35 +0100

    Faulty swagger model

--- txmatching/data_transfer_objects/hla/parsing_issue_swagger.py
+++ txmatching/data_transfer_objects/hla/parsing_issue_swagger.py
@@ -4,9 +4,17 @@ from txmatching.utils.hla_system.hla_transformations.parsing_issue_detail import
     ParsingIssueDetail
 from txmatching.web.web_utils.namespaces import public_api
 
+ParsingIssueBaseJson = public_api.model('ParsingIssueBase', {
+    'hla_code_or_group': fields.String(required=True),
+    'parsing_issue_detail': fields.String(required=True,
+                                          enum=[parsing_issue.name for parsing_issue in ParsingIssueDetail]),
+    'message': fields.String(required=True),
+})
+
 ParsingIssuePublicJson = public_api.model('ParsingIssuePublic', {
     'hla_code_or_group': fields.String(required=True),
-    'parsing_issue_detail': fields.String(required=True, enum=[parsing_issue.name for parsing_issue in ParsingIssueDetail]),
+    'parsing_issue_detail': fields.String(required=True,
+                                          enum=[parsing_issue.name for parsing_issue in ParsingIssueDetail]),
     'message': fields.String(required=True),
     'txm_event_name': fields.String(required=True),
     'medical_id': fields.String(required=True)
