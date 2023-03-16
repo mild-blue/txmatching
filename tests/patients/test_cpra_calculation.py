9b555db8a0b2fb7750b3dcee1df5ba5f4c1aa61f
Jakub Monhart <monhart.jakub@gmail.com>
2023-03-16 15:34:32 +0100

    Decide compatibility based on recipient-donor score.

--- tests/patients/test_cpra_calculation.py
+++ tests/patients/test_cpra_calculation.py
@@ -10,6 +10,7 @@ from txmatching.configuration.config_parameters import ConfigParameters
 from txmatching.patients.patient import (
     Donor, Recipient, TxmEvent)
 from txmatching.patients.recipient_compatibility import calculate_cpra_and_get_compatible_donors_for_recipient
+from txmatching.utils.blood_groups import BloodGroup
 from txmatching.utils.enums import HLACrossmatchLevel
 
 
@@ -19,6 +20,8 @@ class TestCPRACalculation(TestCase):
         self.some_hla_raw_codes = ['A*24:09N', 'A*32:02', 'B*57:16', 'A*02:140', 'A10', 'DR4',
                                    'DRB1*04:10', 'B*44:55', 'DQB1*06:01', 'A*02:08', 'A*02:140',
                                    'DPA1*01:18', 'A32']
+        
+        self.recipient_hla_raw_codes = ['A32', 'B57', 'A2']
 
         # creating recipients for different solutions
         self.recipient_general = self.__create_mock_recipient_object_with_hla_antibodies(
@@ -26,10 +29,12 @@ class TestCPRACalculation(TestCase):
                 raw_code=self.some_hla_raw_codes[1],  # recipient just with A*32:02 antibody
                 mfi=4000,
                 cutoff=2000)]
-            )
+            ),
+            create_hla_typing(self.recipient_hla_raw_codes)
         )
         self.recipient_without_antibodies = self.__create_mock_recipient_object_with_hla_antibodies(
-            create_antibodies(hla_antibodies_list=[])
+            create_antibodies(hla_antibodies_list=[]), 
+            create_hla_typing(self.recipient_hla_raw_codes)
         )
         self.recipient_against_all_donors = self.__create_mock_recipient_object_with_hla_antibodies(
             create_antibodies(hla_antibodies_list=[create_antibody(
@@ -37,9 +42,10 @@ class TestCPRACalculation(TestCase):
                 mfi=4000,
                 cutoff=2000)
                 for raw_code in self.some_hla_raw_codes]
-            )
+            ),
+            create_hla_typing(self.recipient_hla_raw_codes)
         )
-
+        
         self.PatientsTuple = namedtuple(
             typename='patients',
             field_names=['recipients', 'donors']
@@ -62,10 +68,10 @@ class TestCPRACalculation(TestCase):
         )
 
         # creating general config parameters:
-        # The tests DO NOT CONTROL the dependence of cPRA on CONFIGURATION!
-        # If you have changed crossmatch logic (function is_positive_hla_crossmatch()) for some config,
-        # the expected results should be recalculated!
-        self.config_parameters_general = ConfigParameters(use_high_resolution=True, # TODO
+        # The tests DO NOT CONTROL the dependence of cPRA and compatible_donors on CONFIGURATION!
+        # If you have changed crossmatch logic (function is_positive_hla_crossmatch()) for some config 
+        # or scorer's config parameters, the expected results should be recalculated!
+        self.config_parameters_general = ConfigParameters(use_high_resolution=True,
                                                           hla_crossmatch_level=HLACrossmatchLevel.BROAD)
 
     def test_calculate_cpra_for_recipient_general_case(self):
@@ -76,7 +82,7 @@ class TestCPRACalculation(TestCase):
             config_parameters=self.config_parameters_general)
         
         self.assertEqual(
-            (0.25, {3, 4, 5, 6, 7, 8, 9, 10, 11}), (cpra, compatible_donors))
+            (0.25, {3, 4, 9, 10, 11}), (cpra, compatible_donors))
 
     def test_calculate_cpra_for_recipient_without_antibodies_case(self):
         """Case: recipient without antibodies"""
@@ -86,7 +92,7 @@ class TestCPRACalculation(TestCase):
             config_parameters=self.config_parameters_general)
         
         self.assertEqual(
-            (0, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}), (cpra, compatible_donors))
+            (0, {1, 2, 3, 4, 9, 10, 11, 12}), (cpra, compatible_donors))
 
     def test_calculate_cpra_for_recipient_against_all_donors_case(self):
         """Case: recipient is incompatible to all donors in txm_event"""
@@ -103,7 +109,7 @@ class TestCPRACalculation(TestCase):
         txm_event = self.__create_mock_txm_event_object_with_patients(
             self.PatientsTuple(donors=[],
                                recipients=[self.recipient_general]))
-        
+
         cpra, compatible_donors, _ = calculate_cpra_and_get_compatible_donors_for_recipient(
             txm_event=txm_event, recipient=self.recipient_against_all_donors, 
             config_parameters=self.config_parameters_general)
@@ -113,9 +119,11 @@ class TestCPRACalculation(TestCase):
         
     @staticmethod
     @patch(f'{__name__}.Recipient')
-    def __create_mock_recipient_object_with_hla_antibodies(hla_antibodies, mocked_Recipient):
+    def __create_mock_recipient_object_with_hla_antibodies(hla_antibodies, hla_typing, mocked_Recipient):
         recipient = mocked_Recipient.return_value
         recipient.hla_antibodies = hla_antibodies
+        recipient.parameters.hla_typing = hla_typing
+        recipient.parameters.blood_group = BloodGroup.A
         return recipient
 
     @staticmethod
@@ -123,6 +131,7 @@ class TestCPRACalculation(TestCase):
     def __create_mock_donor_object_with_hla_typing(hla_typing, db_id, mocked_Donor):
         donor = mocked_Donor.return_value
         donor.parameters.hla_typing = hla_typing
+        donor.parameters.blood_group = BloodGroup.A
         donor.db_id = db_id
         return donor
 
--- txmatching/patients/recipient_compatibility.py
+++ txmatching/patients/recipient_compatibility.py
@@ -2,6 +2,7 @@ from typing import Callable, List, Set, Tuple
 
 from txmatching.configuration.config_parameters import ConfigParameters
 from txmatching.patients.patient import Recipient, TxmEvent
+from txmatching.scorers.scorer_constants import TRANSPLANT_IMPOSSIBLE_SCORE
 from txmatching.scorers.scorer_from_config import scorer_from_configuration
 from txmatching.utils.blood_groups import blood_groups_compatible
 from txmatching.utils.hla_system.compatibility_index import \
@@ -13,6 +14,8 @@ from txmatching.utils.recipient_donor_compatibility_details import \
     RecipientDonorCompatibilityDetails
 
 
+# pylint: disable=too-many-locals
+# Too many locals might be resolved while optimizing: https://github.com/mild-blue/txmatching/issues/1163
 def calculate_cpra_and_get_compatible_donors_for_recipient(txm_event: TxmEvent,
                                                            recipient: Recipient,
                                                            config_parameters: ConfigParameters,
@@ -22,6 +25,8 @@ def calculate_cpra_and_get_compatible_donors_for_recipient(txm_event: TxmEvent,
     """
     Calculates cPRA for recipient (which part of donors [as decimal] is incompatible) for txm_event and returns list of
     compatible donors optionally with details about compatibility.
+    While cPRA is computed based solely on the hla crossmatch, compatible donors are computed based on the overall
+    recipient-donor score.
 
     :param compatibility_details: Compute details of recipient-donor compatibility if True, return only set of
         compatible donors if False.
@@ -37,6 +42,8 @@ def calculate_cpra_and_get_compatible_donors_for_recipient(txm_event: TxmEvent,
 
     compatible_donors_details = []
     compatible_donors = set()
+    n_hla_crossmatch_compatible = 0
+
     for donor in active_donors:
 
         antibodies = crossmatch_logic(
@@ -48,29 +55,32 @@ def calculate_cpra_and_get_compatible_donors_for_recipient(txm_event: TxmEvent,
                         for antibody_match in antibody_match_group.antibody_matches
                         if antibody_match.match_type.is_positive_for_level(config_parameters.hla_crossmatch_level)}
 
-        if len(common_codes) == 0: # donor is compatible with the recipient
+        if len(common_codes) == 0: # donor is compatible with the recipient (considering only hla crossmatch)
+            n_hla_crossmatch_compatible += 1
+            score = scorer.score_transplant(donor, recipient, None)
 
-            compatible_donors.add(donor.db_id)
+            if score > TRANSPLANT_IMPOSSIBLE_SCORE: # donor is compatible with the recipient
+                compatible_donors.add(donor.db_id)
 
-            # Do not send compatibility info with the original donor
-            if compatibility_details and (donor.related_recipient_db_id != recipient.db_id):
-                compatibility_index_detailed = get_detailed_compatibility_index(
-                    donor_hla_typing=donor.parameters.hla_typing,
-                    recipient_hla_typing=recipient.parameters.hla_typing,
-                    ci_configuration=scorer.ci_configuration
-                )
-                detailed_score = get_detailed_score(compatibility_index_detailed, antibodies)
+                # Do not compute compatibility details with the original donor
+                if compatibility_details and (donor.related_recipient_db_id != recipient.db_id):
+                    compatibility_index_detailed = get_detailed_compatibility_index(
+                        donor_hla_typing=donor.parameters.hla_typing,
+                        recipient_hla_typing=recipient.parameters.hla_typing,
+                        ci_configuration=scorer.ci_configuration
+                    )
+                    detailed_score = get_detailed_score(compatibility_index_detailed, antibodies)
 
-                recipient_donor_compatibility_details = RecipientDonorCompatibilityDetails(
-                    recipient_db_id=recipient.db_id,
-                    donor_db_id=donor.db_id,
-                    score=scorer.score_transplant(donor, recipient, None),
-                    max_score=scorer.max_transplant_score,
-                    compatible_blood=blood_groups_compatible(
-                            donor.parameters.blood_group, recipient.parameters.blood_group),
-                    detailed_score=detailed_score
-                )
+                    recipient_donor_compatibility_details = RecipientDonorCompatibilityDetails(
+                        recipient_db_id=recipient.db_id,
+                        donor_db_id=donor.db_id,
+                        score=score,
+                        max_score=scorer.max_transplant_score,
+                        compatible_blood=blood_groups_compatible(
+                                donor.parameters.blood_group, recipient.parameters.blood_group),
+                        detailed_score=detailed_score
+                    )
 
-                compatible_donors_details.append(recipient_donor_compatibility_details)
+                    compatible_donors_details.append(recipient_donor_compatibility_details)
 
-    return 1 - len(compatible_donors) / len(active_donors), compatible_donors, compatible_donors_details
+    return 1 - n_hla_crossmatch_compatible / len(active_donors), compatible_donors, compatible_donors_details
