031339b68c39d854e3a20f69fc64ba6cfdbfcdb2
Kristina Galikova <kristina.galik@gmail.com>
2023-05-29 16:37:59 +0100

    feat: add new warning to parsing issue detail

---
+++ txmatching/database/db_migrations/0038.add-warning-in-case-of-rare-hla.sql
@@ -0,0 +1,6 @@
+--
+-- file: txmatching/database/db_migrations/0038.add-warning-in-case-of-rare-hla.sql
+-- depends: 0037.add-attribute-to-parsing-issue-detail
+--
+
+ALTER TYPE PARSING_ISSUE_DETAIL ADD VALUE 'CREATED_THEORETICAL_ANTIBODY';
