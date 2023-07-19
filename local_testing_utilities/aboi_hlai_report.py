e68e414da6f817029f63970e982a08c9e8c750c8
Jakub Monhart <monhart.jakub@gmail.com>
2023-07-19 15:24:14 +0200

    Add script to compute number of ABOi HLAi pairs.

---
+++ local_testing_utilities/aboi_hlai_report.py
@@ -0,0 +1,70 @@
+from typing import List
+
+import pandas as pd
+
+from txmatching.database.services.config_service import get_configuration_parameters_from_db_id_or_default
+from txmatching.database.services.txm_event_service import get_txm_event_complete, get_txm_event_db_id_by_name
+from txmatching.utils.blood_groups import blood_groups_compatible
+from txmatching.utils.hla_system.hla_crossmatch import is_positive_hla_crossmatch
+from txmatching.web import create_app
+
+
+def get_n_incompatible_pairs(txm_event_name: str) -> List[str | int]:
+    txm_event_db_id = get_txm_event_db_id_by_name(txm_event_name)
+    txm_event = get_txm_event_complete(txm_event_db_id)
+    config_parameters = get_configuration_parameters_from_db_id_or_default(txm_event, None)
+
+    n_pairs = 0
+    n_aboi_pairs = 0
+    n_hlai_pairs = 0
+    n_aboi_and_hlai_pairs = 0
+
+    for donor in txm_event.all_donors:
+        if donor.related_recipient_db_id and donor.db_id in txm_event.active_and_valid_donors_dict:
+            # Valid donor - related recipient pair
+            n_pairs += 1
+
+            related_recipient = next(recipient for recipient in txm_event.all_recipients if
+                                     recipient.db_id == donor.related_recipient_db_id)
+            is_aboi, is_hlai = False, False
+
+            # Test ABOi
+            if not blood_groups_compatible(donor.parameters.blood_group, related_recipient.parameters.blood_group):
+                is_aboi = True
+                n_aboi_pairs += 1
+
+            # Test HLAi
+            if is_positive_hla_crossmatch(donor_hla_typing=donor.parameters.hla_typing,
+                                          recipient_antibodies=related_recipient.hla_antibodies,
+                                          use_high_resolution=config_parameters.use_high_resolution,
+                                          crossmatch_level=config_parameters.hla_crossmatch_level):
+                is_hlai = True
+                n_hlai_pairs += 1
+
+            if is_aboi and is_hlai:
+                n_aboi_and_hlai_pairs += 1
+
+    n_aboi_or_hlai_pairs = n_aboi_pairs + n_hlai_pairs - n_aboi_and_hlai_pairs
+    return [n_pairs, n_aboi_pairs, n_hlai_pairs, n_aboi_and_hlai_pairs, n_aboi_or_hlai_pairs]
+
+
+if __name__ == '__main__':
+    app = create_app()
+    with app.app_context():
+        # Specify txm events to compute the number of incompatible pairs for:
+        txm_events_names = ['high_res_example_data', 'mock_data_CZE_CAN_IND']
+
+        df = pd.DataFrame(columns=['txm_event_name', 'n_pairs', 'n_aboi_pairs', 'n_hlai_pairs',
+                                   'n_aboi_and_hlai_pairs', 'n_aboi_or_hlai_pairs'])
+
+        for txm_event_name in txm_events_names:
+            df.loc[len(df)] = \
+                [txm_event_name] + get_n_incompatible_pairs(txm_event_name)
+
+    # Sum over all events
+    sum_all_events = df.sum()
+    sum_all_events.txm_event_name = 'all_events'
+    df.loc[len(df)] = sum_all_events
+
+    df.to_csv('aboi_hlai_report'
+              '.csv', index=False)
