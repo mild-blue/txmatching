b1d2c86bc18dcd60810c1b59b9e1f549d521b6f0
Jakub Monhart <monhart.jakub@gmail.com>
2023-07-20 14:14:49 +0200

    Filter only pairs from CZE, ISR, AUT for ABOi HLAi script.

--- local_testing_utilities/aboi_hlai_report.py
+++ local_testing_utilities/aboi_hlai_report.py
@@ -5,6 +5,7 @@ import pandas as pd
 from txmatching.database.services.config_service import get_configuration_parameters_from_db_id_or_default
 from txmatching.database.services.txm_event_service import get_txm_event_complete, get_txm_event_db_id_by_name
 from txmatching.utils.blood_groups import blood_groups_compatible
+from txmatching.utils.country_enum import Country
 from txmatching.utils.hla_system.hla_crossmatch import is_positive_hla_crossmatch
 from txmatching.web import create_app
 
@@ -20,10 +21,12 @@ def get_n_incompatible_pairs(txm_event_name: str) -> List[str | int]:
     n_aboi_and_hlai_pairs = 0
 
     for donor in txm_event.all_donors:
+        if donor.parameters.country_code not in [Country.CZE, Country.ISR, Country.AUT]:
+            continue
+
         if donor.related_recipient_db_id and donor.db_id in txm_event.active_and_valid_donors_dict:
             # Valid donor - related recipient pair
             n_pairs += 1
-
             related_recipient = next(recipient for recipient in txm_event.all_recipients if
                                      recipient.db_id == donor.related_recipient_db_id)
             is_aboi, is_hlai = False, False
@@ -52,8 +55,20 @@ if __name__ == '__main__':
     app = create_app()
     with app.app_context():
         # Specify txm events to compute the number of incompatible pairs for:
-        txm_events_names = ['high_res_example_data', 'mock_data_CZE_CAN_IND']
-
+        txm_events_names = ['36-TXM-2022-01',
+                            '2020-10-CZE-ISR-AUT',
+                            '2021-01-CZE-ISR-AUT',
+                            '2020-07-CZE-ISR-AUT',
+                            '37-TXM-2022-04',
+                            '38-TXM-2022-07',
+                            '33-TXM-2021-04-v2',
+                            '33-TXM-2021-04',
+                            '39-TXM-2022-10',
+                            '34-TXM-2021-07',
+                            '35-TXM-2021-10',
+                            '40-TXM-2023-01',
+                            '41-TXM-2023-04',
+                            '42-TXM-2023-07']
         df = pd.DataFrame(columns=['txm_event_name', 'n_pairs', 'n_aboi_pairs', 'n_hlai_pairs',
                                    'n_aboi_and_hlai_pairs', 'n_aboi_or_hlai_pairs'])
 
