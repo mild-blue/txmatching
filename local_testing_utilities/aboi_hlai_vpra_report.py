57c93916bc1ed640feca7693b8709aa427e79f31
Jakub Monhart <monhart.jakub@gmail.com>
2023-07-20 20:17:25 +0200

    Solve different format of DQA, DPA, DPB in low res.

--- local_testing_utilities/aboi_hlai_vpra_report.py
+++ local_testing_utilities/aboi_hlai_vpra_report.py
@@ -1,20 +1,27 @@
-from typing import List
+# pylint: disable-all
+import csv
 
+import numpy as np
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 from txmatching.database.services.config_service import get_configuration_parameters_from_db_id_or_default
 from txmatching.database.services.txm_event_service import get_txm_event_complete, get_txm_event_db_id_by_name
 from txmatching.utils.blood_groups import blood_groups_compatible
 from txmatching.utils.country_enum import Country
-from txmatching.utils.enums import HLAAntibodyType
+from txmatching.utils.enums import HLAAntibodyType, DQDPChain, HLAGroup
 from txmatching.utils.hla_system.hla_crossmatch import is_positive_hla_crossmatch
 from txmatching.web import create_app
+from txmatching.utils.hla_system.compatibility_index import _which_dq_dp_chain
 
 
-def calculate_n_incompatible_pairs(txm_event_name: str) -> List[str | int]:
-    txm_event_db_id = get_txm_event_db_id_by_name(txm_event_name)
-    txm_event = get_txm_event_complete(txm_event_db_id)
+HIGHLY_SENSITIZED_TH = 90.0
+VERY_HIGHLY_SENSITIZED_TH = 97.0
+
+
+def calculate_n_incompatible_pairs(txm_event):
     config_parameters = get_configuration_parameters_from_db_id_or_default(txm_event, None)
 
     n_pairs = 0
@@ -22,7 +29,8 @@ def calculate_n_incompatible_pairs(txm_event_name: str) -> List[str | int]:
     n_hlai_pairs = 0
     n_aboi_and_hlai_pairs = 0
 
-    for donor in txm_event.all_donors:
+    print('Computing incompatible pairs')
+    for donor in tqdm(txm_event.all_donors):
         if donor.parameters.country_code not in [Country.CZE, Country.ISR, Country.AUT]:
             continue
 
@@ -50,28 +58,7 @@ def calculate_n_incompatible_pairs(txm_event_name: str) -> List[str | int]:
                 n_aboi_and_hlai_pairs += 1
 
     n_aboi_or_hlai_pairs = n_aboi_pairs + n_hlai_pairs - n_aboi_and_hlai_pairs
-    return [n_pairs, n_aboi_pairs, n_hlai_pairs, n_aboi_and_hlai_pairs, n_aboi_or_hlai_pairs]
-
-
-def calculate_highly_sensitized_recipients(txm_event_name):
-    txm_event_db_id = get_txm_event_db_id_by_name(txm_event_name)
-    txm_event = get_txm_event_complete(txm_event_db_id)
-    config_parameters = get_configuration_parameters_from_db_id_or_default(txm_event, None)
-
-    for recipient in txm_event.all_recipients[:2]:
-
-        unacceptable_antibodies = []
-        for antibodies_group in recipient.hla_antibodies.hla_antibodies_per_groups:
-            for antibody in antibodies_group.hla_antibody_list:
-                if antibody.type == HLAAntibodyType.NORMAL and \
-                        antibody.second_code is None and \
-                        antibody.mfi > antibody.cutoff:
-                    unacceptable_antibodies.append(antibody.code.display_code)
-
-        print(f'{recipient.medical_id=}')
-        print(f'{unacceptable_antibodies=}')
-        comput_vpra(unacceptable_antibodies)
-        print()
+    return n_pairs, n_aboi_pairs, n_hlai_pairs, n_aboi_and_hlai_pairs, n_aboi_or_hlai_pairs
 
 
 def comput_vpra(unacceptable_antibodies):
@@ -93,53 +80,122 @@ def comput_vpra(unacceptable_antibodies):
 
     response = requests.post(url, data=body, headers=headers)
     soup = BeautifulSoup(response.content, 'lxml')
-    print(soup.find('frequency'))
+    # print(soup.find('frequency'))
+    vpra = float(soup.find('frequency').get_text())
 
-    print(soup.find('matches'))
+    # print(soup.find('matches'))
 
-    print(soup.find('panelsize'))
+    # print(soup.find('panelsize'))
 
-    print(soup.find('errormessage'))
+    # print(soup.find('errormessage'))
 
-def compute_report():
+    return vpra
 
-    # Specify txm events to compute the number of incompatible pairs for:
-    txm_events_names = ['high_res_example_data']
 
-    calculate_highly_sensitized_recipients(txm_events_names[0])
+def calculate_highly_sensitized_recipients(txm_event):
+    vpras = []
+    print('Computing vpras')
+    for recipient in tqdm(txm_event.all_recipients):
+        if recipient.db_id in txm_event.active_and_valid_recipients_dict:
+            unacceptable_antibodies = []
+            for antibodies_group in recipient.hla_antibodies.hla_antibodies_per_groups:
+                for antibody in antibodies_group.hla_antibody_list:
+                    if antibody.type == HLAAntibodyType.NORMAL and \
+                            antibody.second_code is None and \
+                            antibody.mfi > antibody.cutoff:
 
-    # # Specify txm events to compute the number of incompatible pairs for:
-    # txm_events_names = ['36-TXM-2022-01',
-    #                     '2020-10-CZE-ISR-AUT',
-    #                     '2021-01-CZE-ISR-AUT',
-    #                     '2020-07-CZE-ISR-AUT',
-    #                     '37-TXM-2022-04',
-    #                     '38-TXM-2022-07',
-    #                     '33-TXM-2021-04-v2',
-    #                     '33-TXM-2021-04',
-    #                     '39-TXM-2022-10',
-    #                     '34-TXM-2021-07',
-    #                     '35-TXM-2021-10',
-    #                     '40-TXM-2023-01',
-    #                     '41-TXM-2023-04',
-    #                     '42-TXM-2023-07']
-    # df = pd.DataFrame(columns=['txm_event_name', 'n_pairs', 'n_aboi_pairs', 'n_hlai_pairs',
-    #                            'n_aboi_and_hlai_pairs', 'n_aboi_or_hlai_pairs'])
-    #
-    # for txm_event_name in txm_events_names:
-    #     df.loc[len(df)] = \
-    #         [txm_event_name] + calculate_n_incompatible_pairs(txm_event_name)
-    #     pass
-    #
-    # # Sum over all events
-    # sum_all_events = df.sum()
-    # sum_all_events.txm_event_name = 'all_events'
-    # df.loc[len(df)] = sum_all_events
-    #
-    # df.to_csv('aboi_hlai_report'
-    #           '.csv', index=False)
+                        # Solve different nomenclature of DPA, DQA, DPB
+                        if antibodies_group.hla_group in [HLAGroup.DP, HLAGroup.DQ] and \
+                                not antibody.code.is_in_high_res():
+
+                            dq_dp_chain = _which_dq_dp_chain(antibody)
+
+                            if dq_dp_chain == DQDPChain.BETA_DP:
+                                hla_number = antibody.code.broad[2:]
+                                if len(hla_number) == 1:
+                                    unacceptable_antibodies.append(('DP-0' + hla_number))
+                                else:
+                                    unacceptable_antibodies.append(('DP-' + hla_number))
+
+                            elif dq_dp_chain in [DQDPChain.ALPHA_DP, DQDPChain.ALPHA_DQ]:
+                                hla_number = antibody.code.broad[2:]
+                                if len(hla_number) == 1:
+                                    unacceptable_antibodies.append((dq_dp_chain + '-0' + hla_number))
+                                else:
+                                    unacceptable_antibodies.append((dq_dp_chain + '-' + hla_number))
+
+                            else:
+                                # DQB
+                                unacceptable_antibodies.append(antibody.code.display_code)
+
+                        # All other loci are used with the same nomencalture we use
+                        else:
+                            unacceptable_antibodies.append(antibody.code.display_code)
+
+            # print(f'{recipient.medical_id=}')
+            # print(f'{unacceptable_antibodies=}')
+            breakpoint()
+            # vpras.append(comput_vpra(unacceptable_antibodies))
+            # print()
+
+    n_highly_sensitized = (np.array(vpras) > HIGHLY_SENSITIZED_TH).sum()
+    n_very_highly_sensitized = (np.array(vpras) > VERY_HIGHLY_SENSITIZED_TH).sum()
+    return n_highly_sensitized, n_very_highly_sensitized, vpras
+
+
+def compute_report(txm_events_names):
+
+    df = pd.DataFrame(columns=['txm_event_name', 'n_pairs', 'n_aboi_pairs', 'n_hlai_pairs',
+                               'n_aboi_and_hlai_pairs', 'n_aboi_or_hlai_pairs', 'n_highly_sensitized_recipients',
+                               'n_very_highly_sensitized_recipients'])
+
+    list_of_vpras = []
+
+    for i, txm_event_name in enumerate(txm_events_names):
+        print(f'[{i+1}/{len(txm_events_names)}] Computing report for {txm_event_name}')
+        txm_event_db_id = get_txm_event_db_id_by_name(txm_event_name)
+        txm_event = get_txm_event_complete(txm_event_db_id)
+
+        n_pairs, n_aboi_pairs, n_hlai_pairs, n_aboi_and_hlai_pairs, n_aboi_or_hlai_pairs = \
+            calculate_n_incompatible_pairs(txm_event)
+        n_highly_sensitized, n_very_highly_sensitized, vpras = calculate_highly_sensitized_recipients(txm_event)
+        list_of_vpras.append([txm_event_name] + vpras)
+
+        df.loc[len(df)] = \
+            [txm_event_name, n_pairs, n_aboi_pairs, n_hlai_pairs, n_aboi_and_hlai_pairs, n_aboi_or_hlai_pairs,
+             n_highly_sensitized, n_very_highly_sensitized]
+
+    # Sum over all events
+    sum_all_events = df.sum()
+    sum_all_events.txm_event_name = 'all_events'
+    df.loc[len(df)] = sum_all_events
+
+    df.to_csv('aboi_hlai_vpra_report.csv', index=False)
+
+    with open('vpras.csv', "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(list_of_vpras)
+
 
 if __name__ == '__main__':
+    # Specify txm events to compute the number of incompatible pairs for:
+    # txm_events = ['36-TXM-2022-01',
+    #               '2020-10-CZE-ISR-AUT',
+    #               '2021-01-CZE-ISR-AUT',
+    #               '2020-07-CZE-ISR-AUT',
+    #               '37-TXM-2022-04',
+    #               '38-TXM-2022-07',
+    #               '33-TXM-2021-04-v2',
+    #               '33-TXM-2021-04',
+    #               '39-TXM-2022-10',
+    #               '34-TXM-2021-07',
+    #               '35-TXM-2021-10',
+    #               '40-TXM-2023-01',
+    #               '41-TXM-2023-04',
+    #               '42-TXM-2023-07']
+
+    # txm_events = ['high_res_example_data', 'mock_data_CZE_CAN_IND']
+    txm_events = ['mock_data_CZE_CAN_IND']
     app = create_app()
     with app.app_context():
-        compute_report()
+        compute_report(txm_events)
